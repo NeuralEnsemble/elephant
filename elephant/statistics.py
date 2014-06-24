a9b9bb5e3546e50138290d6cbca0d93656ce10fc
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2014-06-24 16:14:06 +0200

    Extended statistics functions to accept plain NumPy arrays as well as SpikeTrains.

--- elephant/statistics.py
+++ elephant/statistics.py
@@ -9,21 +9,67 @@ import numpy as np
 import quantities as pq
 
 
-def isi(spiketrain):
+def isi(spiketrain, units=None):
     """
-    Return a quantities array containing the inter-spike intervals of the
+    Return an array containing the inter-spike intervals of the
     SpikeTrain.
+
+    Accepts either a Neo SpikeTrain or a plain NumPy array. If either a
+    SpikeTrain is provided or `units` is specified, the return value will
+    be a quantities array, otherwise a plain NumPy array. The units of
+    the quantities array will be the same as `units`, if given, or the
+    units of the SpikeTrain otherwise.
+
+    Parameters
+    ----------
+
+    spiketrain : Neo SpikeTrain or NumPy ndarray containing spike times
+    units : str, optional
+
+    Returns
+    -------
+
+    NumPy array or quantities array.
+
     """
-    return pq.Quantity(np.array(np.diff(spiketrain)), spiketrain.units)
+    intervals = np.diff(spiketrain)
+    if hasattr(spiketrain, "units"):
+        if units is not None:
+            intervals.units = units
+        return pq.Quantity(np.array(intervals), intervals.units)
+    elif units is not None:
+        return pq.Quantity(intervals, units)
+    else:
+        return intervals
 
 
 def cv_isi(spiketrain):
     """
     Return the coefficient of variation of the inter-spike intervals.
+
+    Accepts either a Neo SpikeTrain or a plain NumPy array, containing
+    at least two spike times. If there are fewer than two spikes,
+    raises ValueError.
+
+    Parameters
+    ----------
+
+    spiketrain : Neo SpikeTrain or NumPy ndarray containing spike times
+
+    Returns
+    -------
+
+    float
+
+    Raises
+    ------
+
+    ValueError
+        If the spiketrain contains fewer than two spikes.
     """
     if spiketrain.size > 1:
         intervals = isi(spiketrain)
         return np.std(intervals)/np.mean(intervals)
     else:
-        # should perhaps emit a warning?
-        return np.nan
+        # alternative is to return NaN
+        raise ValueError("Spike train must contain at least two spikes.")
--- elephant/test/test_statistics.py
+++ elephant/test/test_statistics.py
@@ -6,6 +6,7 @@ docstring goes here
 """
 
 import unittest
+import numpy as np
 from numpy.testing.utils import assert_array_almost_equal
 import quantities as pq
 import neo
@@ -14,11 +15,36 @@ import elephant.statistics as es
 
 class SpikeTrainStatisticsTest(unittest.TestCase):
 
-    def test_isi(self):
+    def test_isi_with_spiketrain(self):
         st = neo.SpikeTrain([0.3, 0.56, 0.87, 1.23], units='ms', t_stop=10.0)
         target = pq.Quantity([0.26, 0.31, 0.36], 'ms')
         assert_array_almost_equal(es.isi(st), target, decimal=9)
 
+    def test_isi_with_plain_array_no_units(self):
+        st = np.array([0.3, 0.56, 0.87, 1.23])
+        target = np.array([0.26, 0.31, 0.36])
+        intervals = es.isi(st)
+        assert not isinstance(intervals, pq.Quantity)
+        assert_array_almost_equal(intervals, target, decimal=9)
+
+    def test_isi_with_plain_array_and_units(self):
+        st = np.array([0.3, 0.56, 0.87, 1.23])
+        target = pq.Quantity([0.26, 0.31, 0.36], 'ms')
+        intervals = es.isi(st, units="ms")
+        assert isinstance(intervals, pq.Quantity)
+        assert_array_almost_equal(intervals, target, decimal=9)
+
+    def test_isi_with_spiketrain_change_units(self):
+        st = neo.SpikeTrain([0.3, 0.56, 0.87, 1.23], units='s', t_stop=10.0)
+        target = pq.Quantity([260.0, 310.0, 360.0], 'ms')
+        assert_array_almost_equal(es.isi(st, units='ms'), target, decimal=9)
+
     def test_cv_isi_of_regular_spiketrain_is_zero(self):
         regular_st = neo.SpikeTrain([1, 2, 3, 4, 5], units='ms', t_stop=10.0)
-        self.assertEqual(es.cv_isi(regular_st), 0.0)
+        cvisi = es.cv_isi(regular_st)
+        self.assertEqual(cvisi, 0.0)
+
+    def test_cv_isi_of_regular_array_is_zero(self):
+        regular_st = np.array([1, 2, 3, 4, 5])
+        cvisi = es.cv_isi(regular_st)
+        self.assertEqual(cvisi, 0.0)
