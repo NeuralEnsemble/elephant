0d459cb430f7648a100babcbbbb4083f77c63f47
Todd Jennings <toddrjen@gmail.com>
2014-06-27 00:58:01 +0200

    simplify isi

--- elephant/statistics.py
+++ elephant/statistics.py
@@ -1,32 +1,35 @@
+# -*- coding: utf-8 -*-
 """
-docstring goes here
+docstring goes here.
 
 :copyright: Copyright 2014 by the Elephant team, see AUTHORS.txt.
 :license: Modified BSD, see LICENSE.txt for details.
 """
 
 from __future__ import division, print_function
+
 import numpy as np
-import scipy.stats
 import quantities as pq
+import scipy.stats
 
 
-def isi(spiketrain, units=None):
+def isi(spiketrain, axis=-1):
     """
-    Return an array containing the inter-spike intervals of the
-    SpikeTrain.
+    Return an array containing the inter-spike intervals of the SpikeTrain.
 
-    Accepts either a Neo SpikeTrain or a plain NumPy array. If either a
-    SpikeTrain is provided or `units` is specified, the return value will
+    Accepts a Neo SpikeTrain, a Quantity array, or a plain NumPy array.
+    If either a SpikeTrain or Quantity array is provided, the return value will
     be a quantities array, otherwise a plain NumPy array. The units of
-    the quantities array will be the same as `units`, if given, or the
-    units of the SpikeTrain otherwise.
+    the quantities array will be the same as spiketrain.
 
     Parameters
     ----------
 
-    spiketrain : Neo SpikeTrain or NumPy ndarray containing spike times
-    units : str, optional
+    spiketrain : Neo SpikeTrain or Quantity array or NumPy ndarray
+                 The spike times.
+    axis : int, optional
+           The axis along which the difference is taken.
+           Default is the last axis.
 
     Returns
     -------
@@ -34,15 +37,14 @@ def isi(spiketrain, units=None):
     NumPy array or quantities array.
 
     """
-    intervals = np.diff(spiketrain)
-    if hasattr(spiketrain, "units"):
-        intervals = pq.Quantity(np.array(intervals), units=spiketrain.units)
-        if units is not None:
-            intervals = intervals.rescale(units)
-    elif units is not None:
-        intervals = pq.Quantity(intervals, units)
+    if axis is None:
+        axis = -1
+    intervals = np.diff(spiketrain, axis=axis)
+    if hasattr(spiketrain, 'waveforms'):
+        intervals = pq.Quantity(intervals.magnitude, units=spiketrain.units)
     return intervals
 
 
-# we make `cv` an alias for scipy.stats.variation for the convenience of former NeuroTools users
+# we make `cv` an alias for scipy.stats.variation for the convenience
+# of former NeuroTools users
 cv = scipy.stats.variation
--- elephant/test/test_statistics.py
+++ elephant/test/test_statistics.py
@@ -1,50 +1,94 @@
+# -*- coding: utf-8 -*-
 """
-docstring goes here
+docstring goes here.
 
 :copyright: Copyright 2014 by the Elephant team, see AUTHORS.txt.
 :license: Modified BSD, see LICENSE.txt for details.
 """
 
 import unittest
+
+import neo
 import numpy as np
 from numpy.testing.utils import assert_array_almost_equal
 import quantities as pq
-import neo
+
 import elephant.statistics as es
 
 
-class SpikeTrainStatisticsTest(unittest.TestCase):
+class isi_TestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_array_2d = np.array([[0.3, 0.56, 0.87, 1.23],
+                                       [0.02, 0.71, 1.82, 8.46],
+                                       [0.03, 0.14, 0.15, 0.92]])
+        self.targ_array_2d_0 = np.array([[-0.28,  0.15,  0.95,  7.23],
+                                         [0.01, -0.57, -1.67, -7.54]])
+        self.targ_array_2d_1 = np.array([[0.26, 0.31, 0.36],
+                                         [0.69, 1.11, 6.64],
+                                         [0.11, 0.01, 0.77]])
+        self.targ_array_2d_default = self.targ_array_2d_1
+
+        self.test_array_1d = self.test_array_2d[0, :]
+        self.targ_array_1d = self.targ_array_2d_1[0, :]
 
     def test_isi_with_spiketrain(self):
-        st = neo.SpikeTrain([0.3, 0.56, 0.87, 1.23], units='ms', t_stop=10.0)
-        target = pq.Quantity([0.26, 0.31, 0.36], 'ms')
-        assert_array_almost_equal(es.isi(st), target, decimal=9)
+        st = neo.SpikeTrain(self.test_array_1d, units='ms', t_stop=10.0)
+        target = pq.Quantity(self.targ_array_1d, 'ms')
+        res = es.isi(st)
+        assert_array_almost_equal(res, target, decimal=9)
 
-    def test_isi_with_plain_array_no_units(self):
-        st = np.array([0.3, 0.56, 0.87, 1.23])
-        target = np.array([0.26, 0.31, 0.36])
-        intervals = es.isi(st)
-        assert not isinstance(intervals, pq.Quantity)
-        assert_array_almost_equal(intervals, target, decimal=9)
+    def test_isi_with_quantities_1d(self):
+        st = pq.Quantity(self.test_array_1d, units='ms')
+        target = pq.Quantity(self.targ_array_1d, 'ms')
+        res = es.isi(st)
+        assert_array_almost_equal(res, target, decimal=9)
 
-    def test_isi_with_plain_array_and_units(self):
-        st = np.array([0.3, 0.56, 0.87, 1.23])
-        target = pq.Quantity([0.26, 0.31, 0.36], 'ms')
-        intervals = es.isi(st, units="ms")
-        assert isinstance(intervals, pq.Quantity)
-        assert_array_almost_equal(intervals, target, decimal=9)
+    def test_isi_with_plain_array_1d(self):
+        st = self.test_array_1d
+        target = self.targ_array_1d
+        res = es.isi(st)
+        assert not isinstance(res, pq.Quantity)
+        assert_array_almost_equal(res, target, decimal=9)
 
-    def test_isi_with_spiketrain_change_units(self):
-        st = neo.SpikeTrain([0.3, 0.56, 0.87, 1.23], units='s', t_stop=10.0)
-        target = pq.Quantity([260.0, 310.0, 360.0], 'ms')
-        assert_array_almost_equal(es.isi(st, units='ms'), target, decimal=9)
+    def test_isi_with_plain_array_2d_default(self):
+        st = self.test_array_2d
+        target = self.targ_array_2d_default
+        res = es.isi(st)
+        assert not isinstance(res, pq.Quantity)
+        assert_array_almost_equal(res, target, decimal=9)
 
-    def test_cv_isi_of_regular_spiketrain_is_zero(self):
-        regular_st = neo.SpikeTrain([1, 2, 3, 4, 5], units='ms', t_stop=10.0)
-        cvisi = es.cv(es.isi(regular_st))
-        self.assertEqual(cvisi, 0.0)
+    def test_isi_with_plain_array_2d_0(self):
+        st = self.test_array_2d
+        target = self.targ_array_2d_0
+        res = es.isi(st, axis=0)
+        assert not isinstance(res, pq.Quantity)
+        assert_array_almost_equal(res, target, decimal=9)
 
-    def test_cv_isi_of_regular_array_is_zero(self):
-        regular_st = np.array([1, 2, 3, 4, 5])
-        cvisi = es.cv(es.isi(regular_st))
-        self.assertEqual(cvisi, 0.0)
+    def test_isi_with_plain_array_2d_1(self):
+        st = self.test_array_2d
+        target = self.targ_array_2d_1
+        res = es.isi(st, axis=1)
+        assert not isinstance(res, pq.Quantity)
+        assert_array_almost_equal(res, target, decimal=9)
+
+
+class isi_cv_TestCase(unittest.TestCase):
+    def setUp(self):
+        self.test_array_regular = np.arange(1, 6)
+
+    def test_cv_isi_regular_spiketrain_is_zero(self):
+        st = neo.SpikeTrain(self.test_array_regular,  units='ms', t_stop=10.0)
+        targ = 0.0
+        res = es.cv(es.isi(st))
+        self.assertEqual(res, targ)
+
+    def test_cv_isi_regular_array_is_zero(self):
+        st = self.test_array_regular
+        targ = 0.0
+        res = es.cv(es.isi(st))
+        self.assertEqual(res, targ)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
