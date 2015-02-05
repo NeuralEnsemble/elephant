dcaf7b688d007024eb1f7efad6f99030226ebc7a
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2015-02-05 12:45:19 +0100

    Added `spike_train_generation` module with functions to generate homogeneous Poisson and gamma processes.

--- doc/api_reference.rst
+++ doc/api_reference.rst
@@ -5,3 +5,7 @@ API reference
 
 .. automodule:: elephant.statistics
    :members:
+
+
+.. automodule:: elephant.spike_train_generation
+   :members:
---
+++ elephant/test/test_spike_train_generation.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+docstring goes here.
+
+:copyright: Copyright 2014 by the Elephant team, see AUTHORS.txt.
+:license: Modified BSD, see LICENSE.txt for details.
+"""
+
+from __future__ import division
+import unittest
+
+import neo
+import numpy as np
+from numpy.testing.utils import assert_array_almost_equal
+from scipy.stats import kstest, expon
+from quantities import ms, Hz
+
+import elephant.spike_train_generation as stgen
+from elephant.statistics import isi
+
+
+def pdiff(a, b):
+    """Difference between a and b as a fraction of a
+
+    i.e. abs((a - b)/a)
+    """
+    return abs((a -b)/a)
+
+
+class HomogeneousPoissonProcessTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_statistics(self):
+        # There is a statistical test and has a non-zero chance of failure during normal operation.
+        # Re-run the test to see if the error persists.
+        rate = 123.0*Hz
+        t_stop = 2345*ms
+        spiketrain = stgen.homogeneous_poisson_process(rate, t_stop=t_stop)
+        intervals = isi(spiketrain)
+
+        expected_spike_count = int((rate * t_stop).simplified)
+        self.assertLess(pdiff(expected_spike_count, spiketrain.size), 0.2)  # should fail about 1 time in 1000
+
+        expected_mean_isi = (1/rate).rescale(ms)
+        self.assertLess(pdiff(expected_mean_isi, intervals.mean()), 0.2)
+
+        expected_first_spike = 0*ms
+        self.assertLess(spiketrain[0] - expected_first_spike, 7*expected_mean_isi)
+
+        expected_last_spike = t_stop
+        self.assertLess(expected_last_spike - spiketrain[-1], 7*expected_mean_isi)
+
+        # Kolmogorov-Smirnov test
+        D, p = kstest(intervals, "expon", args=(0, expected_mean_isi),  # args are (loc, scale)
+                      alternative='two-sided')
+        self.assertGreater(p, 0.001)
+        self.assertLess(D, 0.12)
+
+    def test_low_rates(self):
+        spiketrain = stgen.homogeneous_poisson_process(0*Hz, t_stop=1000*ms)
+        self.assertEqual(spiketrain.size, 0)
+        # not really a test, just making sure that all code paths are covered
+        for i in range(10):
+            spiketrain = stgen.homogeneous_poisson_process(1*Hz, t_stop=1000*ms)
+
+    def test_buffer_overrun(self):
+        np.random.seed(6085)  # this seed should produce a buffer overrun
+        t_stop=1000*ms
+        rate = 10*Hz
+        spiketrain = stgen.homogeneous_poisson_process(rate, t_stop=t_stop)
+        expected_last_spike = t_stop
+        expected_mean_isi = (1/rate).rescale(ms)
+        self.assertLess(expected_last_spike - spiketrain[-1], 4*expected_mean_isi)
+
+
+
+class HomogeneousGammaProcessTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_statistics(self):
+        # There is a statistical test and has a non-zero chance of failure during normal operation.
+        # Re-run the test to see if the error persists.
+        a = 3.0
+        b = 67.0*Hz
+        t_stop = 2345*ms
+        spiketrain = stgen.homogeneous_gamma_process(a, b, t_stop=t_stop)
+        intervals = isi(spiketrain)
+
+        expected_spike_count = int((b/a * t_stop).simplified)
+        self.assertLess(pdiff(expected_spike_count, spiketrain.size), 0.25)  # should fail about 1 time in 1000
+
+        expected_mean_isi = (a/b).rescale(ms)
+        self.assertLess(pdiff(expected_mean_isi, intervals.mean()), 0.3)
+
+        expected_first_spike = 0*ms
+        self.assertLess(spiketrain[0] - expected_first_spike, 4*expected_mean_isi)
+
+        expected_last_spike = t_stop
+        self.assertLess(expected_last_spike - spiketrain[-1], 4*expected_mean_isi)
+
+        # Kolmogorov-Smirnov test
+        D, p = kstest(intervals, "gamma", args=(a, 0, (1/b).rescale(t_stop.units)),  # args are (a, loc, scale)
+                      alternative='two-sided')
+        self.assertGreater(p, 0.001)
+        self.assertLess(D, 0.25)
+
+
+if __name__ == '__main__':
+    unittest.main()
