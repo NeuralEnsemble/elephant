3d77829664d544ec4218ab8d745b002f97a598f0
Junji Ito <j.ito@fz-juelich.de>
2015-03-05 13:48:31 +0100

    changed the module name from `signal` to `signal_processing`

--- elephant/test/test_signal.py
+++ elephant/test/test_signal.py
@@ -13,7 +13,7 @@ import scipy.signal as spsig
 import quantities as pq
 import neo.core as n
 
-import elephant.signal
+import elephant.signal_processing
 
 
 class ButterTestCase(unittest.TestCase):
@@ -31,20 +31,20 @@ class ButterTestCase(unittest.TestCase):
         # Note: the default detrend function of scipy.signal.welch() seems to
         # cause artificial finite power at the lowest frequencies. Here I avoid
         # this by using an identity function for detrending
-        filtered_noise = elephant.signal.butter(noise, 250.0 * pq.Hz, None)
+        filtered_noise = elephant.signal_processing.butter(noise, 250.0 * pq.Hz, None)
         _, psd = spsig.welch(filtered_noise, nperseg=1024, fs=1000.0,
                              detrend=lambda x: x)
         self.assertAlmostEqual(psd[0], 0)
 
         # test low-pass filtering: power at the highest frequency
         # should be almost zero
-        filtered_noise = elephant.signal.butter(noise, None, 250.0 * pq.Hz)
+        filtered_noise = elephant.signal_processing.butter(noise, None, 250.0 * pq.Hz)
         _, psd = spsig.welch(filtered_noise, nperseg=1024, fs=1000.0)
         self.assertAlmostEqual(psd[-1], 0)
 
         # test band-pass filtering: power at the lowest and highest frequencies
         # should be almost zero
-        filtered_noise = elephant.signal.butter(noise, 200.0 * pq.Hz,
+        filtered_noise = elephant.signal_processing.butter(noise, 200.0 * pq.Hz,
                                                 300.0 * pq.Hz)
         _, psd = spsig.welch(filtered_noise, nperseg=1024, fs=1000.0,
                              detrend=lambda x: x)
@@ -53,7 +53,7 @@ class ButterTestCase(unittest.TestCase):
 
         # test band-stop filtering: power at the intermediate frequency
         # should be almost zero
-        filtered_noise = elephant.signal.butter(noise, 400.0 * pq.Hz,
+        filtered_noise = elephant.signal_processing.butter(noise, 400.0 * pq.Hz,
                                                 100.0 * pq.Hz)
         _, psd = spsig.welch(filtered_noise, nperseg=1024, fs=1000.0)
         self.assertAlmostEqual(psd[256], 0)
@@ -67,11 +67,11 @@ class ButterTestCase(unittest.TestCase):
         # with filtfunc=filtfilt (i.e. default option)
         kwds = {'anasig': noise, 'highpassfreq': 250.0 * pq.Hz,
                 'lowpassfreq': None, 'filtfunc': 'filtfilt'}
-        filtered_noise = elephant.signal.butter(**kwds)
+        filtered_noise = elephant.signal_processing.butter(**kwds)
         _, psd_filtfilt = spsig.welch(filtered_noise, nperseg=1024, fs=1000.0, detrend=lambda x: x)
 
         kwds['filtfunc'] = 'lfilter'
-        filtered_noise = elephant.signal.butter(**kwds)
+        filtered_noise = elephant.signal_processing.butter(**kwds)
         _, psd_lfilter = spsig.welch(filtered_noise, nperseg=1024, fs=1000.0, detrend=lambda x: x)
 
         self.assertAlmostEqual(psd_filtfilt[0], psd_lfilter[0])
@@ -83,7 +83,7 @@ class ButterTestCase(unittest.TestCase):
         # test exception upon invalid filtfunc string
         kwds = {'anasig': anasig_dummy, 'highpassfreq': 250.0 * pq.Hz,
                 'filtfunc': 'invalid_filter'}
-        self.assertRaises(ValueError, elephant.signal.butter, **kwds)
+        self.assertRaises(ValueError, elephant.signal_processing.butter, **kwds)
 
     def test_butter_missing_cutoff_freqs(self):
         # generate a dummy AnalogSignal
@@ -92,7 +92,7 @@ class ButterTestCase(unittest.TestCase):
         # test a case where no cut-off frequencies are given
         kwds = {'anasig': anasig_dummy, 'highpassfreq': None,
                 'lowpassfreq': None}
-        self.assertRaises(ValueError, elephant.signal.butter, **kwds)
+        self.assertRaises(ValueError, elephant.signal_processing.butter, **kwds)
 
 
 
