f25fa426ecf6f1106e648c5abb8e57b5a0b1533d
Alper <a.yegenoglu@fz-juelich.de>
2017-11-22 18:30:32 +0100

    Update test_script.sh
    small fix

--- continuous_integration/test_script.sh
+++ continuous_integration/test_script.sh
@@ -13,7 +13,7 @@ python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
 
 if [[ "$COVERAGE" == "true" ]]; then
-    if [["$MPI == true" ]]; then
+    if [["$MPI == "true" ]]; then
 	mpiexec -n 2 nosetests --with-coverage --cover-package=elephant
     else
 	nosetests --with-coverage --cover-package=elephant
