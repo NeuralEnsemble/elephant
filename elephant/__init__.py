fae88d8f6615d920132bd3ab89d5876319657082
Michael Denker <m.denker@fz-juelich.de>
2018-04-04 14:20:53 +0200

    Various changes to files and draft of release notes

--- elephant/__init__.py
+++ elephant/__init__.py
@@ -2,7 +2,7 @@
 """
 Elephant is a package for the analysis of neurophysiology data, based on Neo.
 
-:copyright: Copyright 2014-2016 by the Elephant team, see AUTHORS.txt.
+:copyright: Copyright 2014-2018 by the Elephant team, see AUTHORS.txt.
 :license: Modified BSD, see LICENSE.txt for details.
 """
 
@@ -17,6 +17,8 @@ from . import (statistics,
                spike_train_surrogates,
                signal_processing,
                current_source_density,
+               change_point_detection,
+               phase_analysis,
                sta,
                conversion,
                neo_tools,
@@ -28,4 +30,4 @@ try:
 except ImportError:
     pass
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
