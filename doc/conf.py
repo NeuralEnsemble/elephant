52280403c97214c2bec1db461df45ef0f38e2900
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2018-10-19 16:15:59 +0200

    typo

--- doc/conf.py
+++ doc/conf.py
@@ -175,7 +175,7 @@ html_use_index = True
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'elephantdoc'
 
-# Supresses  wrong numpy doc warnings
+# Suppresses  wrong numpy doc warnings
 # see here https://github.com/phn/pytpm/issues/3#issuecomment-12133978
 numpydoc_show_class_members = False
 
