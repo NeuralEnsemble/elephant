36ca6725fe7fe4baec4fdeab7831b8f7674583d6
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2018-12-02 15:20:37 -0500

    Add rauc and derivative to signal_processing

--- elephant/signal_processing.py
+++ elephant/signal_processing.py
@@ -611,3 +611,150 @@ def hilbert(signal, N='nextpow'):
     output = signal.duplicate_with_new_array(
         scipy.signal.hilbert(signal.magnitude, N=n, axis=0)[:n_org])
     return output / output.units
+
+
+def rauc(signal, bin_duration=None, baseline=None, t_start=None, t_stop=None):
+    '''
+    Calculate the rectified area under the curve (RAUC) for an AnalogSignal.
+
+    The signal is optionally divided into bins with duration `bin_duration`, and
+    the rectified signal (absolute value) is integrated within each bin to find
+    the area under the curve. By default, the mean of the signal is subtracted
+    before rectification. If the number of bins is 1 (default), a single value
+    is returned for each channel in the input signal. Otherwise, an AnalogSignal
+    containing the values for each bin is returned along with the times of the
+    centers of the bins.
+
+    Parameters
+    ----------
+    signal : neo.AnalogSignal
+        The signal to integrate. If `signal` contains more than one channel,
+        each is integrated separately.
+    bin_duation : quantities.Quantity
+        The length of time that each integration should span. If None, there
+        will be only one bin spanning the entire signal duration. If
+        `bin_duration` does not divide evenly into the signal duration, the end
+        of the signal is padded with zeros to accomodate the final,
+        overextending bin.
+        Default: None
+    baseline : quantities.Quantity
+        A factor to subtract from the signal before rectification. If None, the
+        mean value of the entire signal is used.
+        Default: None
+    t_start, t_stop : quantities.Quantity
+        Times to start and end the algorithm. The signal is cropped using
+        `signal.time_slice(t_start, t_stop)` after baseline removal. Useful if
+        you want the RAUC for a short section of the signal but want the
+        automatic mean calculation (`baseline=None`) to use the entire signal
+        for better baseline estimation.
+        Default: None
+
+    Returns
+    -------
+    quantities.Quantity or neo.AnalogSignal
+        If the number of bins is 1, the returned object is a scalar or
+        vector Quantity containing a single RAUC value for each channel.
+        Otherwise, the returned object is an AnalogSignal containing the
+        RAUC(s) for each bin stored as a sample, with times corresponding to
+        the center of each bin. The output signal will have the same number
+        of channels as the input signal.
+
+    Raises
+    ------
+    TypeError
+        If the input signal is not a neo.AnalogSignal.
+    TypeError
+        If `bin_duation` is not None or a Quantity.
+    TypeError
+        If `baseline` is not None or a Quantity.
+    '''
+
+    if not isinstance(signal, neo.AnalogSignal):
+        raise TypeError('Input signal is not a neo.AnalogSignal!')
+
+    if baseline is not None:
+        # subtract arbitrary baseline
+        if isinstance(baseline, pq.Quantity):
+            signal = signal - baseline
+        else:
+            raise TypeError('baseline must be a Quantity: {}'.format(baseline))
+    else:
+        # subtract mean from each channel
+        signal = signal - signal.mean(axis = 0)
+
+    # slice the signal after subtracting baseline
+    signal = signal.time_slice(t_start, t_stop)
+
+    if bin_duration is not None:
+        # from an arbitrary bin duration, determine samples per bin and number of bins
+        if isinstance(bin_duration, pq.Quantity):
+            samples_per_bin = int(np.round(bin_duration.rescale('s') / signal.sampling_period.rescale('s')))
+            n_bins = int(np.ceil(signal.shape[0] / samples_per_bin))
+        else:
+            raise TypeError('bin_duration must be a Quantity: {}'.format(bin_duration))
+    else:
+        # all samples in one bin
+        samples_per_bin = signal.shape[0]
+        n_bins = 1
+
+    # store the actual bin duration
+    bin_duration = samples_per_bin * signal.sampling_period.rescale('s')
+
+    # reshape into equal size bins, padding the end with zeros if necessary
+    n_channels = signal.shape[1]
+    sig_binned = signal.as_quantity().copy()
+    sig_binned.resize(n_bins * samples_per_bin, n_channels)
+    sig_binned = sig_binned.reshape(n_bins, samples_per_bin, n_channels)
+
+    # rectify and integrate over each bin
+    rauc = np.trapz(np.abs(sig_binned), dx = signal.sampling_period, axis = 1)
+
+    if n_bins == 1:
+        # return a single value for each channel
+        return rauc.squeeze()
+
+    else:
+        # return an AnalogSignal with times corresponding to the center of each bin
+        rauc_sig = neo.AnalogSignal(
+            rauc,
+            t_start = signal.t_start.rescale('s') + bin_duration/2,
+            sampling_period = bin_duration,
+        )
+        return rauc_sig
+
+
+def derivative(signal):
+    '''
+    Calculate the derivative of an AnalogSignal.
+
+    Parameters
+    ----------
+    signal : neo.AnalogSignal
+        The signal to differentiate. If `signal` contains more than one channel,
+        each is differentiated separately.
+
+    Returns
+    -------
+    neo.AnalogSignal
+        The returned object is an AnalogSignal containing the differences
+        between each successive sample value of the input signal divided by the
+        sampling period. Times are centered between the successive samples of
+        the input. The output signal will have the same number of channels as
+        the input signal.
+
+    Raises
+    ------
+    TypeError
+        If the input signal is not a neo.AnalogSignal.
+    '''
+
+    if not isinstance(signal, neo.AnalogSignal):
+        raise TypeError('Input signal is not a neo.AnalogSignal!')
+
+    derivative_sig = neo.AnalogSignal(
+        np.diff(signal.as_quantity(), axis=0) / signal.sampling_period,
+        t_start = signal.t_start + signal.sampling_period / 2,
+        sampling_period = signal.sampling_period,
+    )
+
+    return derivative_sig
