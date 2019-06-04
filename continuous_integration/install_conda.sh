431b3d43e2739fc611dff768c68d352d1e35924b
dizcza <dizcza@gmail.com>
2019-06-04 10:43:23 +0200

    fix travis.yml

--- continuous_integration/install_conda.sh
+++ continuous_integration/install_conda.sh
@@ -6,8 +6,6 @@
 # The behavior of the script is controlled by environment variabled defined
 # in the .travis.yml in the top level folder of the project.
 
-set -e
-
 # Use the miniconda installer for faster download / install of conda
 # itself
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
@@ -16,15 +14,14 @@ export MINICONDA_PATH=$HOME/miniconda
 bash miniconda.sh -b -p ${MINICONDA_PATH}
 export PATH=${MINICONDA_PATH}/bin:$PATH
 conda config --set always_yes yes
-conda update --yes conda
+conda update conda
 
-conda install python=${TRAVIS_PYTHON_VERSION} coveralls
-pip install -r requirements.txt
-conda install --yes --no-update-dependencies mkl
+conda install python==${TRAVIS_PYTHON_VERSION}
+conda config --append channels conda-forge
+sed -i 's/^neo/python-neo/g' requirements.txt
+conda install --file requirements.txt
+conda install --no-update-dependencies mkl
 
-if [[ "${DISTRIB}" == "extra" ]]; then
-    pip install -r requirements-extras.txt
-fi
-
-# todo do we need this?
-pip install .
+#if [[ "${DISTRIB}" == "extra" ]]; then
+#    conda install --file requirements-extras.txt
+#fi
