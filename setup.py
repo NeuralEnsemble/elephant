ce76806beeff39fe1ad7dd9d8061528a7a5e2f00
dizcza <dizcza@gmail.com>
2019-06-14 12:01:05 +0200

    inlcude test/*.txt in a pypi tarball

--- setup.py
+++ setup.py
@@ -25,8 +25,7 @@ setup(
         os.path.join('current_source_density_src', '*.py'),
         os.path.join('spade_src', '*.py'),
         os.path.join('spade_src', 'LICENSE'),
-        os.path.join('spade_src', '*.so'),
-        os.path.join('spade_src', '*.pyd')
+        os.path.join('test', '*.txt')
     ]},
 
     install_requires=install_requires,
