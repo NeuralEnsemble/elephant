3c3057445534e996145ac196f29784069f2e8bc9
Danylo Ulianych <dizcza@gmail.com>
2019-07-10 18:21:37 +0200

    Butterworth supports sosfiltfilt filter_function (#234)
    * Butterworth supports sosfiltfilt filter_function
    
    * higher order filters comment

--- elephant/test/test_signal_processing.py
+++ elephant/test/test_signal_processing.py
@@ -380,13 +380,18 @@ class ButterTestCase(unittest.TestCase):
         self.assertAlmostEqual(psd[0, 256], 0)
 
     def test_butter_filter_function(self):
+        """
+        `elephant.signal_processing.butter` return values test for all
+        available filters (result has to be almost equal):
+            * lfilter
+            * filtfilt
+            * sosfiltfilt
+        """
         # generate white noise AnalogSignal
         noise = neo.AnalogSignal(
             np.random.normal(size=5000),
             sampling_rate=1000 * pq.Hz, units='mV')
 
-        # test if the filter performance is as well with filftunc=lfilter as
-        # with filtfunc=filtfilt (i.e. default option)
         kwds = {'signal': noise, 'highpass_freq': 250.0 * pq.Hz,
                 'lowpass_freq': None, 'filter_function': 'filtfilt'}
         filtered_noise = elephant.signal_processing.butter(**kwds)
@@ -398,7 +403,13 @@ class ButterTestCase(unittest.TestCase):
         _, psd_lfilter = spsig.welch(
             filtered_noise.T, nperseg=1024, fs=1000.0, detrend=lambda x: x)
 
+        kwds['filter_function'] = 'sosfiltfilt'
+        filtered_noise = elephant.signal_processing.butter(**kwds)
+        _, psd_sosfiltfilt = spsig.welch(
+            filtered_noise.T, nperseg=1024, fs=1000.0, detrend=lambda x: x)
+
         self.assertAlmostEqual(psd_filtfilt[0, 0], psd_lfilter[0, 0])
+        self.assertAlmostEqual(psd_filtfilt[0, 0], psd_sosfiltfilt[0, 0])
 
     def test_butter_invalid_filter_function(self):
         # generate a dummy AnalogSignal
