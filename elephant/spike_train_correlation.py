1dbf505c7e1c3a784fa134155e789f8fee2d60bb
Alexander van Meegen <a.van.meegen@fz-juelich.de>
2019-11-11 16:56:10 +0100

    Use cross_corr_coef of CCH function

--- elephant/spike_train_correlation.py
+++ elephant/spike_train_correlation.py
@@ -795,21 +795,14 @@ def spike_train_timescale(binned_st, tau_max):
         Slow fluctuations in recurrent networks of spiking neurons.
         Physical Review E, 92(4), 040901.
     """
-    time_interval = binned_st.t_stop - binned_st.t_start
     binsize = binned_st.binsize
-
-    rate = binned_st._sparse_mat_u.sum() / time_interval
-
     if not (tau_max/binsize).units == pq.dimensionless:
         raise AssertionError("tau_max needs units of time")
     tau_max_bins = int(np.round((tau_max/binsize).simplified.magnitude))
     cch_window = [-tau_max_bins, tau_max_bins]
-    cch, bin_ids = cross_correlation_histogram(binned_st, binned_st,
-                                               window=cch_window)
-    # CCH is dimensionless. Should have dimension 1/time^2, thus 1/dt^2.
-    # Furthermore, a multiplicative factor dt/time_interval from convolution.
-    # Subtract the squared first moment to arrive at the correlation function.
-    corrfct = cch / binsize / time_interval - rate**2
+    corrfct, bin_ids = cross_correlation_histogram(
+        binned_st, binned_st, window=cch_window, cross_corr_coef=True
+    )
     # Take only t > 0 values, in particular neglecting the delta peak.
     corrfct_pos = corrfct.time_slice(binsize/2, corrfct.t_stop).flatten()
 
