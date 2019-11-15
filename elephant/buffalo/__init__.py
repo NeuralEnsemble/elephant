31ddb73cd3fded17ef7816fe6dd2b772fc3c0d93
Cristiano Köhler <c.koehler@fz-juelich.de>
2019-11-15 13:23:23 +0100

    Basic implementation of Buffalo objects superclass, supporting automatic validation of inputs and storage of provenance data. Implemented unit tests for checking this basic behavior and prevent implementation errors

---
+++ elephant/buffalo/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Buffalo is a package that implements electrophysiology analysis objects based on Elephant.
+
+:copyright: Copyright 2014-2019 by the Elephant team, see `doc/authors.rst`.
+:license: BSD, see LICENSE.txt for details.
+"""
+
+from . import (base,
+               exceptions,
+               helpers)
