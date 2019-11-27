86effb772437b79e980d198708d2c4e045d2759a
Michael Denker <m.denker@fz-juelich.de>
2019-11-27 22:59:57 +0100

    Demo passing a spike train object

--- elephant/parallel.py
+++ elephant/parallel.py
@@ -1,10 +1,15 @@
 """
 This package contains an implementation of embarassingly parallel processing
-of Elephant functionality in a worker-slave manner.
+of Elephant functionalities, e.g., to perform analysis in sliding windows.
 """
 
+import sys
+import quantities as pq
+import elephant
 from mpi4py import MPI
 
+
+# TODO: Rename as stampede?
 
 class parallel_context():
     """
@@ -18,46 +23,86 @@ class parallel_context():
         run in parallel, you can create multiple communicators to separate the
         individual processes.
         Default: None
-    slave_ranks:
-        List of ranks to be used as slaves within the communicator. If None,
+    worker_ranks:
+        List of ranks to be used as workers within the communicator. If None,
         all N-1 ranks of the current communicator, hosting N ranks, will be
         used.
         Default: None
     """
 
-    def __init__(self, comm=None, slave_ranks=None):
+    def __init__(self, comm=None, worker_ranks=None):
         # Save communicator
-        if not comm:
+        if comm is None:
             self.comm = MPI.COMM_WORLD
         else:
             self.comm = comm
 
         # Get size of current MPI communicator
-        self.comm_size = MPI.COMM_WORLD.Get_size()
+        self.comm_size = self.comm.Get_size()
 
         # Save ranks for slaves
-        if not slave_ranks:
-            self.slave_ranks = range(1, self.comm_size)
+        if not worker_ranks:
+            self.worker_ranks = range(1, self.comm_size)
         else:
-            slave_ranks = set(slave_ranks)
-            if (len(slave_ranks > self.comm_size-1) or
-                    max(slave_ranks) > self.comm_size or
-                    min(slave_ranks) < 1):
+            worker_ranks = set(worker_ranks)
+            if (len(worker_ranks) > self.comm_size-1 or
+                    max(worker_ranks) > self.comm_size or
+                    min(worker_ranks) < 1):
                 raise ValueError(
-                    "Elements of slave_ranks must be >0 and <N, "
+                    "Elements of worker_ranks must be >0 and <N, "
                     "where N is the communicator size.")
-            self.slave_ranks = slave_ranks
+            self.worker_ranks = worker_ranks
 
+        # Save status and name of the rank
         self.status = MPI.Status()
         self.rank_name = MPI.Get_processor_name()
+        self.rank = self.comm.Get_rank()
+        #self.attributes = self.comm.Get_attr()
+
+        # If this is the master node or any node not in the current
+        # communicator, continue with main program. Otherwise start a worker.
+        if self.rank in worker_ranks:
+            self.__run_worker()
+
+    def terminate(self):
+        """
+        This function initializes the MPI subsystem.
+
+        Parameters:
+        comm:
+            MPI communicator to use. If None is given, MPI_COMM_WORLD is used,
+            i.e., all available MPI resources and the global communicator is
+            used. When combining Elephant with other libraries or applications
+            that also run in parallel, you can create multiple communicators to
+            separate the individual processes.
+            Default: None
+        worker_ranks:
+            List of ranks to be used as slaves within the communicator. If 
+            None, all N-1 ranks of the current communicator, hosting N ranks,
+            will be used.
+            Default: None
+        """
+        pass
+
+    def __run_worker(self):
+        while True:
+            req = self.comm.irecv(source=0, tag=11)
+            data = req.wait()
+            print(data)
+        sys.exit(0)
 
 
 def main():
-    pc = parallel_context()
-
+    pc = parallel_context(worker_ranks=[1, 2, 3])
     print("%s, %i" % (pc.rank_name, pc.comm_size))
 
+    s = [elephant.spike_train_generation.homogeneous_poisson_process(
+        10*pq.Hz, t_start=0*pq.s, t_stop=20*pq.s)
+        for _ in range(pc.comm_size-1)]
+
+    for i in range(pc.comm_size-1):
+        req = pc.comm.isend(s[i], i+1, tag=11)
+        req.wait()
 
 if __name__ == "__main__":
     main()
-    
