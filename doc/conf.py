10e9fe4a17becd0fe10026a4d74f8ee483885d5f
Michael Denker <m.denker@fz-juelich.de>
2020-02-27 12:56:32 +0100

    Testing for Alabaster theme

--- doc/conf.py
+++ doc/conf.py
@@ -123,7 +123,10 @@ autosummary_generate = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
+html_theme_options = {
+    'font_family': 'Arial',
+    'body_text':'gray'}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
