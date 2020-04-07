f365e720aa29f5af22ae6ceaf8afdcc368b0004a
Cristiano Köhler <c.koehler@fz-juelich.de>
2020-04-07 15:41:32 +0200

    Fixed Buffalo imports after merge

--- elephant/buffalo/__init__.py
+++ elephant/buffalo/__init__.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
 """
-Buffalo is a package that implements electrophysiology analysis objects to produce standardized outputs and provenance
+Buffalo is a package that implements electrophysiology analysis objects to
+produce standardized outputs and provenance
 capture during the analysis workflow in Elephant.
 
 :copyright: Copyright 2014-2019 by the Elephant team, see `doc/authors.rst`.
 :license: BSD, see LICENSE.txt for details.
 """
 
+from .graph import BuffaloProvGraph
+from .prov import BuffaloProvDocument
+from .provenance import BuffaloProvObject, Provenance
+from . import objects
+
 __version__ = '0.0.2'
+
+__all__ = [BuffaloProvGraph, BuffaloProvDocument, BuffaloProvObject,
+           Provenance, objects]
