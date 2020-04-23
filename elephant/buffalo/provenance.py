14eaf74a5d0e4b4087b8a21b0f4979364d85a2d2
Cristiano Köhler <c.koehler@fz-juelich.de>
2020-04-23 10:26:13 +0200

    Updated provenance object hash method to use joblib.hash for the value

--- elephant/buffalo/provenance.py
+++ elephant/buffalo/provenance.py
@@ -9,9 +9,9 @@ using the W3C PROV standard.
 
 from functools import wraps
 import inspect
+import joblib
 import ast
 from collections import namedtuple
-from collections.abc import Iterable
 
 from elephant.buffalo.prov import BuffaloProvDocument
 from elephant.buffalo.graph import BuffaloProvGraph
@@ -22,11 +22,14 @@ from pprint import pprint
 AnalysisStep = namedtuple('AnalysisStep', ('function',
                                            'input',
                                            'params',
-                                           'output'))
+                                           'output',
+                                           'arg_map',
+                                           'kwarg_map',
+                                           'call_ast'))
+
 
 FunctionDefinition = namedtuple('FunctionDefinition', ('name',
                                                        'module',
-                                                       'qualname',
                                                        'version'))
 
 
@@ -46,12 +49,7 @@ class BuffaloProvObject(object):
         self._id, self._type, self._value = self._get_object_info(obj)
 
     def __hash__(self):
-        if "__hash__" in dir(self._value):
-            value = self._value if self._value.__hash__ is not None \
-                else self.__repr__()
-        else:
-            value = self.__repr__()
-        return hash((self._id, self._type, value))
+        return hash((self._id, self._type, joblib.hash(self._value)))
 
     def __eq__(self, other):
         if isinstance(other, BuffaloProvObject):
@@ -69,10 +67,7 @@ class BuffaloProvObject(object):
         return "{}: {} = {}".format(self._id, self._type, self._value)
 
     def get_md_string(self):
-        if isinstance(self._value, Iterable):
-            value = "(id: {})".format(self._id)
-        else:
-            value = self._value.__repr__()
+        value = "(id: {})".format(self._id)
         return '{}["{}<br>{}"]\n'.format(hash(self), self._type, value)
 
 
@@ -96,33 +91,49 @@ class Provenance(object):
         def wrapped(*args, **kwargs):
             function_output = function(*args, **kwargs)
 
-            # frame = inspect.getouterframes(inspect.currentframe())[1]
-            # tree = ast.parse(frame.code_context[0].strip())
-            # print(ast.dump(tree))
-
             # If capturing provenance...
             if Provenance.active:
 
-                # 1. Extract function name
+                # 1. Capture AST of the call to the function
+
+                frame = inspect.getouterframes(inspect.currentframe())[1]
+                tree = ast.parse(frame.code_context[0].strip())
+
+                # 2. Extract function name and information
+                # TODO: fetch version information
+
                 function_name = FunctionDefinition(
-                    function.__name__, function.__module__,
-                    function.__qualname__, None)
+                    function.__name__, function.__module__, None)
 
-                # 2. Extract parameters passed to function and store in
+                # 3. Extract parameters passed to function and store in
                 #    `input_data` dictionary
+                #    Two separate lists with the names according to the
+                #    arg/kwarg order are also constructed, to map to the
+                #    `args` and `keywords` fields of AST nodes
 
-                # 2.1 Positional arguments
+                # 3.1 Positional arguments
+
                 input_data = {}
-                input_syntax = {}
+                input_args_names = []
                 params = tuple(inspect.signature(function).parameters.keys())
                 for arg_id, arg_val in enumerate(args):
                     arg_name = params[arg_id]
                     input_data[arg_name] = arg_val
+                    input_args_names.append(arg_name)
 
-                # 2.2 Add keyword arguments
-                input_data.update(kwargs)
+                # 3.2 Add keyword arguments
 
-                # 3. Create parameters/input description for the graph
+                input_kwargs_names = []
+                for kwarg_id, kwarg_name in enumerate(kwargs.keys()):
+                    input_data[kwarg_name] = kwargs[kwarg_name]
+                    input_kwargs_names.append(kwarg_name)
+
+                # 4. Create parameters/input descriptions for the graph
+                #    Here the inputs, but not the parameters passed to the
+                #    function are transformed in the hashable type
+                #    BuffaloProvObject. Inputs are defined as the parameter
+                #    `inputs` when initializing the class, and stored as the
+                #    class attribute `inputs`
 
                 parameters = {}
                 inputs = {}
@@ -132,12 +143,21 @@ class Provenance(object):
                     else:
                         parameters[key] = value
 
+                # 5. Create hashable BuffaloProvObject for the output
+
                 output = self.add(function_output)
 
-                # 4. Create tuple with the analysis step information
-                step = AnalysisStep(function_name, inputs, parameters, output)
+                # TODO: do static analysis
+                # self._insert_static_information(tree, inputs, output)
 
-                # 5. Add to history graph / PROV document
+                # 6. Create tuple with the analysis step information
+
+                step = AnalysisStep(function_name, inputs, parameters, output,
+                                    input_args_names, input_kwargs_names,
+                                    tree)
+
+                # 7. Add to history graph / PROV document
+
                 Provenance.history.append(step)
                 Provenance.prov_document.add_step(step)
 
