12b1d93895c8bec62f07ef3f2232720ab6f58fe3
Danylo Ulianych <dizcza@gmail.com>
2020-05-11 13:18:22 -0500

    Fix kernels and statistics (instantaneous_rate & mean_firing_rate) (#313)
    * removed magic in boundary_enclosing_area_fraction
    
    * standardized quantities handling in kernels _evaluate()
    
    * fixed kernels.median_index() and statistics.instantaneous_rate()
    
    * CDF, ICDF, median index
    
    * center_kernel feature

--- doc/reference/toctree/kernels/elephant.kernels.RectangularKernel.rst
+++ doc/reference/toctree/kernels/elephant.kernels.RectangularKernel.rst
@@ -4,5 +4,29 @@ elephant.kernels.RectangularKernel
 .. currentmodule:: elephant.kernels
 
 .. autoclass:: RectangularKernel
-    :members: __call__, is_symmetric, boundary_enclosing_area_fraction, min_cutoff
+   :members:
+   :inherited-members:
+
+   
+   .. rubric:: Methods
+
+   .. autosummary::
+   
+      ~RectangularKernel.__call__
+      ~RectangularKernel.boundary_enclosing_area_fraction
+      ~RectangularKernel.cdf
+      ~RectangularKernel.icdf
+      ~RectangularKernel.is_symmetric
+      ~RectangularKernel.median_index
+   
+   
+
+   
+   
+   .. rubric:: Attributes
+
+   .. autosummary::
+   
+      ~RectangularKernel.min_cutoff
+   
    
--- elephant/utils.py
+++ elephant/utils.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function, unicode_literals
 
 import numpy as np
+import quantities as pq
 
 
 def is_binary(array):
@@ -12,8 +13,31 @@ def is_binary(array):
     Returns
     -------
     bool
-        Whether the input array is binary or nor.
+        Whether the input array is binary or not.
 
     """
     array = np.asarray(array)
     return ((array == 0) | (array == 1)).all()
+
+
+def is_time_quantity(x, allow_none=False):
+    """
+    Parameters
+    ----------
+    x : array-like
+        A scalar or array-like to check for being a Quantity with time units.
+    allow_none : bool
+        Allow `x` to be None or not.
+
+    Returns
+    -------
+    bool
+        Whether the input is a time Quantity (True) or not (False).
+        If the input is None and `allow_none` is set to True, returns True.
+
+    """
+    if x is None and allow_none:
+        return True
+    if not isinstance(x, pq.Quantity):
+        return False
+    return x.dimensionality.simplified == pq.Quantity(1, "s").dimensionality
