f65489157ceaec646f507ae20c7560669b0d4980
Aitor <a.morales-gregorio@fz-juelich.de>
2020-05-19 12:54:30 +0200

    correct PEP8

--- elephant/sta.py
+++ elephant/sta.py
@@ -350,8 +350,8 @@ def spike_triggered_population_response(spiketrains,
 
     References
     ----------
-    .. [1] Okun, M., Steinmetz, N., Cossell, L. et al. Diverse coupling of neurons
-    to populations in sensory cortex. Nature 521, 511–515 (2015).
+    .. [1] Okun, M., Steinmetz, N., Cossell, L. et al. Diverse coupling of
+    neurons to populations in sensory cortex. Nature 521, 511–515 (2015).
     https://doi.org/10.1038/nature14273
     """
     # TODO write docstring
