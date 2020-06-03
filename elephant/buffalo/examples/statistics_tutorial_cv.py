172f22c1d4e2d26d8114dc488551369fe70b908a
Cristiano Köhler <c.koehler@fz-juelich.de>
2020-06-03 17:40:32 +0200

    Created script version of CV-ISI code of the statistics tutorial notebook

---
+++ elephant/buffalo/examples/statistics_tutorial_cv.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from quantities import ms, s, Hz
+from elephant.spike_train_generation import (homogeneous_poisson_process,
+                                             homogeneous_gamma_process)
+
+# Provenance tracking
+from elephant.buffalo import provenance
+
+provenance.activate()
+
+spiketrain_list = [
+     homogeneous_poisson_process(rate=10.0*Hz, t_start=0.0*s, t_stop=100.0*s)
+     for i in range(100)]
+
+plt.figure(dpi=150)
+plt.eventplot(spiketrain_list, linelengths=0.75, linewidths=0.75, color='black')
+plt.xlabel("Time, s")
+plt.ylabel("Neuron id")
+plt.xlim([0, 1])
+
+from elephant.statistics import isi, cv
+cv_list = [cv(isi(spiketrain)) for spiketrain in spiketrain_list]
+
+# let's plot the histogram of CVs
+plt.figure(dpi=100)
+plt.hist(cv_list)
+plt.xlabel('CV')
+plt.ylabel('count')
+plt.title("Coefficient of Variation of homogeneous Poisson process")
+
+# plt.show()
+
+provenance.print_history()
