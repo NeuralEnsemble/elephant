f76f27a0453f1861e805bae537194ca52e9ddbcc
Regimantas Jurkus <regimantas.jurkus@gmail.com>
2020-06-26 16:21:07 +0200

    Added unit tests for the two ValueError exceptions in granger.py

--- elephant/test/test_causality.py
+++ elephant/test/test_causality.py
@@ -93,6 +93,26 @@ class PairwiseGrangerTestCase(unittest.TestCase):
         self.assertEqual(analog_signal_causality.total_interdependence,
                          self.causality.total_interdependence)
 
+    @unittest.skipUnless(sys.version_info >= (2, 7),
+                         "requires Python 2.7 or above")
+    def test_lag_covariances_error(self):
+        """
+        Check that if a signal length is shorter than the set max_lag, a
+        ValueError is raised.
+        """
+        short_signals = np.array([[1, 2], [3, 4]])
+        with self.assertRaises(ValueError):
+            elephant.causality.granger._lag_covariances(
+                short_signals, dimension=2, max_lag=3)
+
+    @unittest.skipUnless(sys.version_info >= (2, 7),
+                         "requires Python 2.7 or above")
+    def test_pairwise_granger_error(self):
+        null_signals = np.array([[0, 0], [0, 0]])
+        with self.assertRaises(ValueError):
+            elephant.causality.granger.pairwise_granger(
+                null_signals, max_order=2)
+
     @unittest.skipUnless(sys.version_info >= (3, 1),
                          "requires Python 3.1 or above")
     def test_result_namedtuple(self):
