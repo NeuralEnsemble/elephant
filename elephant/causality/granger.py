7ff6d3891aa6a5a8648b80e5e74eba479d48e9ac
Regimantas Jurkus <regimantas.jurkus@gmail.com>
2020-07-17 14:11:08 +0200

    Changed the reference keys to the agreed schema.

--- elephant/causality/granger.py
+++ elephant/causality/granger.py
@@ -13,7 +13,7 @@ Granger causality
 Granger causality is a method to determine causal influence of one signal on
 another based on autoregressive modelling. It was developed by Nobel prize
 laureate Clive Granger and has been adopted in various numerical fields ever
-since :cite:`granger-granger1969investigating`. In its simplest form, the
+since :cite:`granger-Granger69_424`. In its simplest form, the
 method tests whether the past values of one signal help to reduce the
 prediction error of another signal, compared to the past values of the latter
 signal alone. If it does reduce the prediction error, the first signal is said
@@ -24,12 +24,12 @@ Limitations
 The user must be mindful of the method's limitations, which are assumptions of
 covariance stationary data, linearity imposed by the underlying autoregressive
 modelling as well as the fact that the variables not included in the model will
-not be accounted for :cite:`granger-seth2007granger`.
+not be accounted for :cite:`granger-Seth07_1667`.
 
 Implementation
 ++++++++++++++
 The mathematical implementation of Granger causality methods in this module
-closely follows :cite:`granger-ding2006granger`.
+closely follows :cite:`granger-Ding06_0608035`.
 
 
 Overview of Functions
