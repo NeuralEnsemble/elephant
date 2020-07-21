9ee8075f03e6717c869eb8da08a9395a586fec84
Cristiano Köhler <c.koehler@fz-juelich.de>
2020-07-21 11:14:19 +0200

    Updated statistics tutorial example script

--- elephant/buffalo/examples/statistics_tutorial_cv.py
+++ elephant/buffalo/examples/statistics_tutorial_cv.py
@@ -11,17 +11,19 @@ from elephant.buffalo import provenance
 provenance.activate()
 
 spiketrain_list = [
-     homogeneous_poisson_process(rate=10.0*Hz, t_start=0.0*s, t_stop=100.0*s)
-     for i in range(100)]
+    homogeneous_poisson_process(rate=10.0*Hz, t_start=0.0*s, t_stop=100.0*s)
+    for i in range(100)]
 
 plt.figure(dpi=150)
-plt.eventplot(spiketrain_list, linelengths=0.75, linewidths=0.75, color='black')
+plt.eventplot(spiketrain_list, linelengths=0.75, linewidths=0.75,
+              color='black')
 plt.xlabel("Time, s")
 plt.ylabel("Neuron id")
 plt.xlim([0, 1])
 
 from elephant.statistics import isi, cv
-cv_list = [cv(isi(spiketrain)) for spiketrain in spiketrain_list]
+cv_list = [cv(isi(spiketrain)) for spiketrain in
+           spiketrain_list]
 
 # let's plot the histogram of CVs
 plt.figure(dpi=100)
@@ -30,6 +32,7 @@ plt.xlabel('CV')
 plt.ylabel('count')
 plt.title("Coefficient of Variation of homogeneous Poisson process")
 
-# plt.show()
+plt.show()
 
 provenance.print_history()
+provenance.save_graph('elephant_tutorial.html', show=True)
