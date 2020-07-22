8b5749f32de3b1f8ec5e345a92476467a4fc04d7
Cristiano Köhler <c.koehler@fz-juelich.de>
2020-07-22 19:22:13 +0200

    Added level sorting to the graph

--- elephant/buffalo/graph.py
+++ elephant/buffalo/graph.py
@@ -3,7 +3,8 @@ from pyvis.network import Network
 
 class BuffaloProvenanceGraph(nx.DiGraph):
 
-    def add_input_to_output(self, analysis_step, input_obj, edge_label, multi_output, **attrs):
+    def _add_input_to_output(self, analysis_step, input_obj, edge_label,
+                             multi_output, **attrs):
         self.add_node(input_obj, label=input_obj.type)
 
         if multi_output:
@@ -20,20 +21,22 @@ class BuffaloProvenanceGraph(nx.DiGraph):
         for key, obj in analysis_step.output.items():
             self.add_node(obj, label=obj.type)
         multi_output = len(list(analysis_step.output.keys())) > 1
+
         edge_attr = analysis_step.params
         edge_label = analysis_step.function.name
+
         if analysis_step.function.module:
             edge_label = analysis_step.function.module + "." + edge_label
 
         for key, obj in analysis_step.input.items():
             if isinstance(obj, VarArgs):
                 for var_arg in obj.value:
-                    self.add_input_to_output(analysis_step, var_arg,
-                                             edge_label, multi_output,
-                                             **edge_attr, **attr)
+                    self._add_input_to_output(analysis_step, var_arg,
+                                              edge_label, multi_output,
+                                              **edge_attr, **attr)
             else:
-                self.add_input_to_output(analysis_step, obj, edge_label,
-                                         multi_output, **edge_attr, **attr)
+                self._add_input_to_output(analysis_step, obj, edge_label,
+                                          multi_output, **edge_attr, **attr)
 
     def to_pyvis(self, filename, show=False, layout=True):
         """
@@ -58,12 +61,29 @@ class BuffaloProvenanceGraph(nx.DiGraph):
 
         def add_node(node_id):
             attr = nodes[node_id]
-            level = attr.get('level', 1)
+            level = attr.get('level', None)
             net.add_node(hash(node_id), title=attr['label'],
-                         label=_get_last_name(attr['label']))#, level=level)
+                         label=_get_last_name(attr['label']), level=level)
 
         edges = self.edges.data()
         nodes = self.nodes
+
+        # Go through the graph from the root(s), to set the levels
+        roots = [node for node, degree in self.in_degree() if degree == 0]
+        for root in roots:
+            nodes[root]['level'] = 0
+            level = 1
+            children = list(self.succ[root])
+            while len(children) > 0:
+                next_children = list()
+                for node in children:
+                    if nodes[node].get('level', 0) < level:
+                        nodes[node]['level'] = level
+                    next_children += list(self.succ[node])
+                level += 1
+                children = next_children
+
+        print(roots)
         # for node, degree in self.in_degree:
         #     if degree == 0:
         #         nodes[node]['level'] = 0
