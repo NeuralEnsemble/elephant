95e32ce74877b911d01b0ae6633fa6edde6cab12
Cristiano Köhler <c.koehler@fz-juelich.de>
2020-07-22 20:43:52 +0200

    Improved edges and nodes labels

--- elephant/buffalo/graph.py
+++ elephant/buffalo/graph.py
@@ -1,42 +1,67 @@
 import networkx as nx
 from pyvis.network import Network
 
+
 class BuffaloProvenanceGraph(nx.DiGraph):
 
     def _add_input_to_output(self, analysis_step, input_obj, edge_label,
-                             multi_output, **attrs):
-        self.add_node(input_obj, label=input_obj.type)
+                             edge_title, multi_output, **attrs):
+        obj_type = input_obj.type
+        self.add_node(input_obj, label=obj_type,
+                      title=obj_type.split(".")[-1])
 
         if multi_output:
             for output_key, output_obj in analysis_step.output.items():
                 self.add_edge(input_obj, output_obj, label=edge_label,
-                              **attrs)
+                              title=edge_title, **attrs)
         else:
             self.add_edge(input_obj, analysis_step.output[0], label=edge_label,
-                          **attrs)
+                          title=edge_title, **attrs)
+
+    @staticmethod
+    def _get_edge_attrs_and_labels(analysis_step):
+        edge_attr = analysis_step.params
+        edge_label = analysis_step.function.name
+        edge_title = edge_label
+
+        if edge_label in ['attribute', 'subscript']:
+            if edge_label == 'attribute':
+                edge_label = ".{}".format(edge_attr['name'])
+            elif edge_label == 'subscript':
+                if 'slice' in edge_attr:
+                    edge_label = str(edge_attr['slice'])
+                else:
+                    edge_label = "[{}]".format(edge_attr['index'])
+        else:
+            if analysis_step.function.module:
+                edge_title = analysis_step.function.module + "." + edge_title
+            edge_label = edge_title.split(".")[-1]
+
+        return edge_attr, edge_label, edge_title
 
     def add_step(self, analysis_step, **attr):
         from elephant.buffalo.provenance import VarArgs
 
         for key, obj in analysis_step.output.items():
-            self.add_node(obj, label=obj.type)
+            obj_type = obj.type
+            self.add_node(obj, label=obj_type.split(".")[-1],
+                          title=obj_type)
         multi_output = len(list(analysis_step.output.keys())) > 1
 
-        edge_attr = analysis_step.params
-        edge_label = analysis_step.function.name
-
-        if analysis_step.function.module:
-            edge_label = analysis_step.function.module + "." + edge_label
+        edge_attr, edge_label, edge_title = \
+            self._get_edge_attrs_and_labels(analysis_step)
 
         for key, obj in analysis_step.input.items():
             if isinstance(obj, VarArgs):
                 for var_arg in obj.value:
                     self._add_input_to_output(analysis_step, var_arg,
-                                              edge_label, multi_output,
-                                              **edge_attr, **attr)
+                                              edge_label, edge_title,
+                                              multi_output, **edge_attr,
+                                              **attr)
             else:
                 self._add_input_to_output(analysis_step, obj, edge_label,
-                                          multi_output, **edge_attr, **attr)
+                                          edge_title, multi_output,
+                                          **edge_attr, **attr)
 
     def to_pyvis(self, filename, show=False, layout=True):
         """
@@ -56,14 +81,12 @@ class BuffaloProvenanceGraph(nx.DiGraph):
             Default: True.
 
         """
-        def _get_last_name(name):
-            return name.split(".")[-1]
 
         def add_node(node_id):
             attr = nodes[node_id]
             level = attr.get('level', None)
-            net.add_node(hash(node_id), title=attr['label'],
-                         label=_get_last_name(attr['label']), level=level)
+            net.add_node(hash(node_id), title=attr['title'],
+                         label=attr['label'], level=level)
 
         edges = self.edges.data()
         nodes = self.nodes
@@ -83,12 +106,13 @@ class BuffaloProvenanceGraph(nx.DiGraph):
                 level += 1
                 children = next_children
 
-        net = Network(height="960px", width="1280px", directed=True, layout=layout)
+        net = Network(height="960px", width="1280px", directed=True,
+                      layout=layout)
         for v, u, edge_attr in edges:
             add_node(v)
             add_node(u)
-            net.add_edge(hash(v), hash(u), title=edge_attr['label'],
-                         label=_get_last_name(edge_attr['label']))
+            net.add_edge(hash(v), hash(u), title=edge_attr['title'],
+                         label=edge_attr['label'])
         net.save_graph(filename)
         if show:
             net.show(name=filename)
