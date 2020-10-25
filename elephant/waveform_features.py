60e79c3d92a64da96df473110a056900a96ae93a
dizcza <dizcza@gmail.com>
2020-10-25 10:52:28 +0100

    waveform_snr works directly with waveforms

--- elephant/waveform_features.py
+++ elephant/waveform_features.py
@@ -28,8 +28,9 @@ def waveform_width(waveform, cutoff=0.75):
 
     Parameters
     ----------
-    waveform : np.ndarray or list or pq.Quantity
-        Time course of a single waveform
+    waveform : array-like
+        Time course of a single waveform. Accepts a list, a numpy array or a
+        quantity.
     cutoff : float, optional
         Defines the normalized range `[0, cutoff]` of the input sequence for
         computing the minimum. Must be in `[0, 1)` range.
@@ -65,9 +66,10 @@ def waveform_width(waveform, cutoff=0.75):
     return width
 
 
-def waveform_snr(spiketrain):
+def waveform_snr(waveforms):
     """
-    Return the signal-to-noise ratio of the waveforms of a SpikeTrain.
+    Return the signal-to-noise ratio of the waveforms of one or more
+    spike trains.
 
     Signal-to-noise ratio is defined as the difference in mean peak-to-trough
     voltage divided by twice the mean SD. The mean SD is computed by
@@ -76,47 +78,48 @@ def waveform_snr(spiketrain):
 
     Parameters
     ----------
-    spiketrain : neo.SpikeTrain
-        The spike times with attached waveforms.
+    waveforms : array-like
+        A list or a quantity or a numpy array of waveforms of shape
+        ``(n_waveforms, time)`` in case of a single spike train or
+        ``(n_waveforms, n_spiketrains, time)`` in case of one or more spike
+        trains.
 
     Returns
     -------
-    snr : float
-        signal-to-noise ratio according to [1]_
+    snr : float or np.ndarray
+        Signal-to-noise ratio according to [1]_. If the input `waveforms`
+        shape is ``(n_waveforms, time)`` or ``(n_waveforms, 1, time)``, a
+        single float is returned. Otherwise, if the shape is
+        ``(n_waveforms, n_spiketrains, time)``, a numpy array of length
+        ``n_spiketrains`` is returned.
 
-    Raises
-    ------
-    ValueError
-        If `spiketrain` has no attached waveforms.
+    Notes
+    -----
+    The waveforms of a `neo.SpikeTrain` can be extracted as
+    `spiketrain.waveforms`, if it's loaded from a file, in which case you need
+    to set ``load_waveforms=True`` in ``neo.read_block()``.
 
     References
     ----------
-
     .. [1] Hatsopoulos, N. G., Xu, Q. & Amit, Y.
            Encoding of Movement Fragments in the Motor Cortex.
            J. Neurosci. 27, 5105–5114 (2007).
 
     """
-    # check whether spiketrain contains waveforms
-    if spiketrain.waveforms is None:
-        raise ValueError('There are no waveforms attached to this \
-                         neo.Spiketrain. Did you forget to set \
-                         load_waveforms=True in neoIO.read_block()?')
-
+    # asarray removes quantities, if present
+    waveforms = np.squeeze(np.asarray(waveforms))
     # average over all waveforms for each bin
-    mean_waveform = np.mean(spiketrain.waveforms.magnitude, axis=0)[0]
-    # standard deviation over all waveforms for each bin
-    std_waveform = np.std(spiketrain.waveforms.magnitude, axis=0)[0]
-    mean_std = np.mean(std_waveform)
+    mean_waveform = waveforms.mean(axis=0)
+    # standard deviation over all waveforms over all bins
+    std_waveform = waveforms.std(axis=0).mean(axis=-1)
 
-    # signal
-    peak_to_trough_voltage = np.max(mean_waveform) - np.min(mean_waveform)
+    # peak to trough voltage signal
+    peak_range = mean_waveform.max(axis=-1) - mean_waveform.min(axis=-1)
     # noise
-    noise = 2 * mean_std
+    noise = 2 * std_waveform
 
-    if noise == 0:
-        warnings.warn('The noise was evaluated to 0.')
-        snr = np.nan
-    else:
-        snr = peak_to_trough_voltage / noise
+    snr = peak_range / noise
+    if np.isnan(snr).any():
+        warnings.warn('The waveforms noise was evaluated to 0. Returning NaN')
+
     return snr
