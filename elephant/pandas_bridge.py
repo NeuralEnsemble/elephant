943fbb1b2322ac2dc439742142167a825f26adf2
dizcza <dizcza@gmail.com>
2020-11-30 22:41:21 +0100

    pandas bridge

--- elephant/pandas_bridge.py
+++ elephant/pandas_bridge.py
@@ -2,6 +2,17 @@
 """
 Bridge to the pandas library.
 
+.. autosummary::
+    :toctree: toctree/pandas_bridge
+
+    spiketrain_to_dataframe
+    event_to_dataframe
+    epoch_to_dataframe
+    multi_spiketrains_to_dataframe
+    multi_events_to_dataframe
+    multi_epochs_to_dataframe
+    slice_spiketrain
+
 :copyright: Copyright 2014-2016 by the Elephant team, see `doc/authors.rst`.
 :license: Modified BSD, see LICENSE.txt for details.
 """
