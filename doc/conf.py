42a7bdcfeacd17463aefda0d4111cffd277d5283
dizcza <dizcza@gmail.com>
2020-12-10 12:32:35 +0100

    never execute notebooks REVERT ME

--- doc/conf.py
+++ doc/conf.py
@@ -113,7 +113,7 @@ pygments_style = 'sphinx'
 #modindex_common_prefix = []
 
 # Only execute Jupyter notebooks that have no evaluated cells
-nbsphinx_execute = 'auto'
+nbsphinx_execute = 'never'
 # Kernel to use for execution
 nbsphinx_kernel_name = 'python3'
 # Cancel compile on errors in notebooks
