81d39df260d80a6f4c718c0cd64f7af4e3fb01bc
dizcza <dizcza@gmail.com>
2020-12-11 13:00:45 +0100

    cubic example

--- elephant/cubic.py
+++ elephant/cubic.py
@@ -11,17 +11,33 @@ cumulants of the population count.
 
 Examples
 --------
-Given a list sts of SpikeTrains, the analysis comprises the following
-steps:
+Homogeneous Poisson random spike trains population count histogram third
+cumulant is explained by the first correlation order (xi=1).
+
+Given a list of spike trains, the analysis comprises the following steps:
 
 1) compute the population histogram (PSTH) with the desired bin size
-       >>> bin_size = 5 * pq.ms
-       >>> pop_count = elephant.statistics.time_histogram(sts, bin_size)
+
+>>> import numpy as np
+>>> import quantities as pq
+>>> from elephant import statistics
+>>> from elephant.cubic import cubic
+>>> from elephant.spike_train_generation import homogeneous_poisson_process
+
+>>> np.random.seed(10)
+>>> spiketrains = [homogeneous_poisson_process(rate=10*pq.Hz,
+...                t_stop=10 * pq.s) for _ in range(20)]
+>>> pop_count = statistics.time_histogram(spiketrains, bin_size=0.1 * pq.s)
 
 2) apply CuBIC to the population count
-       >>> alpha = 0.05  # significance level of the tests used
-       >>> xi, p_val, k = cubic(data, max_iterations=100, alpha=0.05,
-       ... errorval=4.):
+
+>>> xi, p_val, kappa, test_aborted = cubic(pop_count, alpha=0.05)
+>>> xi
+1
+>>> p_val
+[0.43014065113883904]
+>>> kappa
+[20.1, 22.656565656565657, 27.674706246134818]
 
 :copyright: Copyright 2014-2020 by the Elephant team, see `doc/authors.rst`.
 :license: BSD, see LICENSE.txt for details.
@@ -30,6 +46,7 @@ steps:
 from __future__ import division, print_function, unicode_literals
 
 import math
+import numpy as np
 import warnings
 
 import scipy.special
@@ -92,14 +109,13 @@ def cubic(histogram, max_iterations=100, alpha=0.05):
         Whether the test was aborted because reached the maximum number of
         iteration, `max_iterations`.
     """
-    # alpha in in the interval [0,1]
     if alpha < 0 or alpha > 1:
-        raise ValueError(
-            'the significance level alpha (= %s) has to be in [0,1]' % alpha)
+        raise ValueError(f'the significance level alpha ({alpha}) has to be '
+                         f'in [0, 1] range')
 
     if not isinstance(max_iterations, int) or max_iterations < 0:
-        raise ValueError("'max_iterations' ({}) has to be a positive integer"
-                         .format(max_iterations))
+        raise ValueError(f"'max_iterations' ({max_iterations}) has to be a "
+                         "positive integer")
 
     # dict of all possible rate functions
     try:
@@ -120,8 +136,8 @@ def cubic(histogram, max_iterations=100, alpha=0.05):
     while pval < alpha:
         xi_hat = xi
         if xi > max_iterations:
-            warnings.warn('Test aborted, xihat= %i > ximax= %i' % (
-                xi, max_iterations))
+            warnings.warn(f'Test aborted after ximax={max_iterations} '
+                          f'iterations with p-value={pval}')
             test_aborted = True
             break
 
@@ -157,9 +173,11 @@ def _H03xi(kappa, xi, L):
 
     # Check the order condition of the cumulants necessary to perform CuBIC
     if kappa[1] < kappa[0]:
-        raise ValueError(
-            'H_0 can not be tested:'
-            'kappa(2) = %f < %f = kappa(1)!!!' % (kappa[1], kappa[0]))
+        raise ValueError(f"The null hypothesis H_0 cannot be tested: the "
+                         f"population count histogram variance ({kappa[1]}) "
+                         f"is less than the mean ({kappa[0]}). This can "
+                         f"happen when the spike train population is not "
+                         f"large enough or the bin size is small.")
     else:
         # computation of the maximized cumulants
         kstar = [_kappamstar(kappa[:2], i, xi) for i in range(2, 7)]
