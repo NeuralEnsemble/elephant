6276f06433be327466462397ede8cca60ac4c581
Cristiano Köhler <c.koehler@fz-juelich.de>
2021-03-01 18:03:25 +0100

    Documentation and shape validation

--- elephant/buffalo/objects/spectral.py
+++ elephant/buffalo/objects/spectral.py
@@ -2,15 +2,41 @@ from copy import deepcopy
 from .base import AnalysisObject
 from collections import namedtuple
 
-_PSDObjectTuple = namedtuple('PSDObject', 'freqs psd')
+_PSDObjectTuple = namedtuple('PSDObject', 'frequencies psd')
+
 
 class PSDObject(AnalysisObject, _PSDObjectTuple):
-    """AnalysisObject"""
+    """
+    Class to store outputs of Elephant functions that compute power spectrum
+    density (PSD) estimations (e.g.: `elephant.spectral.welch_psd`).
 
-    def __new__(cls, freqs, psd, method, params=None, *args, **kwargs):
-        return super().__new__(cls, freqs, psd)
+    Parameters
+    ----------
+    frequencies : (F,) array-like
+        Vector with the frequencies for which the PSD was computed.
+    psd : (N, F) array-like or (F,) array-like
+        Value of the PSD for each frequency. If multichannel data, `N` is the
+        number of channels and `F` the number of frequencies. Dimension `F`
+        must be the same as `frequencies`.
+    method : str
+        Name of the method that was used to compute the PSD.
+    params : dict, optional
+        All the parameters that were used to compute the PSD.
+        Default: None
 
-    def __init__(self, freqs, psd, method, params=None, *args, **kwargs):
-        super().__init__(self, freqs, psd)
+    Raises
+    ------
+    ValueError
+        If the dimension `F` of `frequencies` and `psd` differ.
+    """
+
+    def __new__(cls, frequencies, psd, method, params=None, *args, **kwargs):
+        if psd.shape[-1] != psd.shape[0]:
+            raise ValueError("PSD array must have the same length for the"
+                             "frequency dimension")
+        return super().__new__(cls, frequencies, psd)
+
+    def __init__(self, frequencies, psd, method, params=None):
+        super().__init__(self, frequencies, psd)
         self.method = method
         self.params = deepcopy(params)
