285b3caeecbccf912b82f2a6a93a9bd545cab167
Michael Denker <m.denker@fz-juelich.de>
2021-08-13 12:32:58 +0200

    Added deprecated fake_neo objects from neo to allow neo 0.10.0+.

--- elephant/test/test_pandas_bridge.py
+++ elephant/test/test_pandas_bridge.py
@@ -15,7 +15,7 @@ from itertools import chain
 
 import numpy as np
 import quantities as pq
-from neo.test.generate_datasets import fake_neo
+from elephant.test.generate_datasets import fake_neo
 from numpy.testing import assert_array_equal
 
 try:
