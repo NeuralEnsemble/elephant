385c150e54dbd65fb33b9d5184db846410867856
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-03-15 15:57:02 +0100

    added classifiers to setup.py (python >=3.7), removed classifiers for python 2

--- setup.py
+++ setup.py
@@ -74,7 +74,10 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Scientific/Engineering']
 )
