348adf61f6f8bcaf667eb363e00ed06f164042b9
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-07-07 13:46:18 +0200

    update spade doctests

--- elephant/spade.py
+++ elephant/spade.py
@@ -67,21 +67,22 @@ bin (i.e., detecting only synchronous patterns).
 Time for data mining: ...
 Time for pvalue spectrum computation: ...
 
->>> patterns[0]['itemset']
-(0, 1, 2, 3, 4, 5)
->>> print(patterns[0]['windows_ids'])  # noqa
-(8, 15, 53, 91, 142, 148, 164, 255, 312, 343, 347, 360, 380, 382, 393, 455, 466)
->>> patterns[0]['times']
-array([  80.,  150.,  530.,  910., 1420., 1480., 1640., 2550., 3120.,
-       3430., 3470., 3600., 3800., 3820., 3930., 4550., 4660.]) * ms
->>> patterns[0]['neurons']
-[0, 1, 2, 3, 4, 5]
->>> patterns[0]['lags']
-array([0., 0., 0., 0., 0.]) * ms
->>> patterns[0]['signature']
-(6, 17)
->>> patterns[0]['pvalue']
-0.0
+>>> patterns[0]
+{'itemset': (4, 3, 0, 2, 5, 1),
+ 'windows_ids': (9,
+  16,
+  55,
+  91,
+  ...,
+  393,
+  456,
+  467),
+ 'neurons': [4, 3, 0, 2, 5, 1],
+ 'lags': array([0., 0., 0., 0., 0.]) * ms,
+ 'times': array([  90.,  160.,  550.,  910.,  930., 1420., 1480., 1650., 2570.,
+        3130., 3430., 3480., 3610., 3800., 3830., 3930., 4560., 4670.]) * ms,
+ 'signature': (6, 18),
+ 'pvalue': 0.0}
 
 
 Refer to Viziphant documentation to check how to visualzie such patterns.
