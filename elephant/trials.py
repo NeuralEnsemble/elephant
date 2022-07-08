f35c78832f1beb655f1544a4269e96736afcfabb
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-07-08 22:07:21 +0200

    fix indents

--- elephant/trials.py
+++ elephant/trials.py
@@ -90,7 +90,6 @@ class TrialsFromBlock(Trials):
     def n_trials(self):
         return len(self.block.segments)
 
-
     def cut_trials(self, reset_time=True):
         """
         cut trials
@@ -100,12 +99,14 @@ class TrialsFromBlock(Trials):
         reset_time : bool, optional
             Reset time base for each trial, so each trial starts at 0 seconds
         """
+        # Create epochs
+        cut_epochs = neo.utils.add_epoch(
+            self.block.segments[0],
+            event1=self.cut_events, event2=None,
+            pre=self.pre, post=self.post,
+            attach_result=False,
+            name='trial_epochs')
 
-        cut_epochs = neo.utils.add_epoch(self.block.segments[0],
-                                     event1=self.cut_events, event2=None,
-                                     pre=self.pre, post=self.post,
-                                     attach_result=False,
-                                     name='trial_epochs')
         # Create the new block
         trials_block = neo.Block()
 
