64c354163a25ed4174f398fcff376e2ee670c9a6
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-07-18 17:10:19 +0200

    skip doctest spike_train_correlation.py

--- elephant/spike_train_correlation.py
+++ elephant/spike_train_correlation.py
@@ -692,7 +692,7 @@ def cross_correlation_histogram(
     [ 5.  3.  3.  2.  4.  0.  1.  5.  3.  4.  2.  2.  2.  5.
       1.  2.  4.  2. -0.  3.  3.] dimensionless
 
-    >>> lags
+    >>> lags # doctest: +SKIP
     array([-10,  -9,  -8,  -7,  -6,  -5,  -4,  -3,  -2,  -1,
          0,   1,   2,   3,   4,   5,   6,   7,   8,   9,
         10], dtype=int32)
