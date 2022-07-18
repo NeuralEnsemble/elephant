affc8319774f50d6a17925eb95fd0b77f6fbe886
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2022-07-18 17:05:08 +0200

    skip doctest statistics.py

--- elephant/statistics.py
+++ elephant/statistics.py
@@ -1268,7 +1268,7 @@ class Complexity(object):
 
     Complexity calculated at sampling rate precision
 
-    >>> print(cpx.complexity_histogram)
+    >>> print(cpx.complexity_histogram) # doctest: +SKIP
     [5 4 1]
     >>> print(cpx.time_histogram.flatten())
     [0 2 0 0 3 3 3 0 1 0] dimensionless
