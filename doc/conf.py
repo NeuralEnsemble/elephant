a37f507d969fb1601d4de90bb180de811bf9515b
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2023-02-17 16:35:56 +0100

    fix broken link in style guide

--- doc/conf.py
+++ doc/conf.py
@@ -150,7 +150,7 @@ html_theme_options = {
 # html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = None
+# html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
