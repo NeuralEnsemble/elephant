c7ef7f94087cb6434d084b4e0ada2b417c0d2b63
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2023-03-23 12:13:41 +0100

    fixed formatting issues in phase_analysis.py

--- elephant/phase_analysis.py
+++ elephant/phase_analysis.py
@@ -352,8 +352,9 @@ def weighted_phase_lag_index(signal_i, signal_j, sampling_frequency=None,
         Takes the absolute value of the numerator in the WPLI-formula.
         When set to `False`, the WPLI contains additional directionality
         information about which signal leads/lags the other signal:
-            - wpli > 0 : first signal i leads second signal j
-            - wpli < 0 : first signal i lags second signal j
+
+        * wpli > 0 : first signal i leads second signal j
+        * wpli < 0 : first signal i lags second signal j
 
     Returns
     -------
@@ -378,12 +379,12 @@ def weighted_phase_lag_index(signal_i, signal_j, sampling_frequency=None,
         WPLI = \frac{| E( |Im(X)| * sgn(Im(X)) ) |}{E( |Im(X)| )}
 
     with:
-        - :math:`E{...}` : expected value operator
-        - :math:`Im{X}` : imaginary component of the cross-spectrum
-        - :math:`X = Z_i Z_{j}^{*}` : cross-spectrum, averaged across
-        trials
-        - :math:`Z_i, Z_j`: complex-valued matrix, representing the Fourier
-        spectra of a particular frequency of the signals i and j.
+
+    * :math:`E{...}` : expected value operator
+    * :math:`Im{X}` : imaginary component of the cross-spectrum
+    * :math:`X = Z_i Z_{j}^{*}` : cross-spectrum, averaged across trials
+    * :math:`Z_i, Z_j`: complex-valued matrix, representing the Fourier
+      spectra of a particular frequency of the signals i and j.
 
     """
     if isinstance(signal_i, neo.AnalogSignal) and \
