044dc3e7e175f1868d6523fe76344b77ac720da7
Felician Richter <f.richter@tiplu.de>
2023-04-26 01:39:03 +0200

    Feat: Add basic scaffold for total_spiking_probability_edges

---
+++ elephant/functional_connectivity_src/total_spiking_probability_edges.py
@@ -0,0 +1,23 @@
+def total_spiking_probability_edges():
+    """
+    Performs the Total spiking probability edges (TSPE) :cite:`tspe-de_blasi2007_???` on spiketrains ...
+
+    Parameters
+    ----------
+    spiketrains: neo.spiketrains
+    Returns
+    -------
+    tspe_matrix
+    """
+
+    tspe_matrix = filter_edges()
+
+    return tspe_matrix
+
+
+def normalized_cross_correlation():
+    pass
+
+
+def filter_edges():
+    pass
