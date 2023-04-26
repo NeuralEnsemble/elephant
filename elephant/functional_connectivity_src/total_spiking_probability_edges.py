c9d7a58097804c80f8ea9c25bf30c04f9500bc6f
Felician Richter <s190655@th-ab.de>
2023-04-26 03:34:16 +0200

    Feat: Add total_spiking_probability_edges

--- elephant/functional_connectivity_src/total_spiking_probability_edges.py
+++ elephant/functional_connectivity_src/total_spiking_probability_edges.py
@@ -1,24 +1,68 @@
 import itertools
-from typing import Iterable, List, NamedTuple, Union
+from typing import Iterable, List, NamedTuple, Union, Optional
 
 import numpy as np
+from scipy.signal import oaconvolve
 
 from elephant.conversion import BinnedSpikeTrain
 
 
-def total_spiking_probability_edges():
+def total_spiking_probability_edges(
+    spike_trains: BinnedSpikeTrain,
+    a: Optional[List[int]] = None,
+    b: Optional[List[int]] = None,
+    c: Optional[List[int]] = None,
+    max_delay: int = 25,
+    normalize: bool = False
+        ):
     """
     Performs the Total spiking probability edges (TSPE) :cite:`tspe-de_blasi2007_???` on spiketrains ...
 
     Parameters
     ----------
     spiketrains: neo.spiketrains
+
     Returns
     -------
     tspe_matrix
     """
 
-    tspe_matrix = filter_edges()
+    if not a:
+        a = [3, 4, 5, 6, 7, 8]
+
+    if not b:
+        b = [2, 3, 4, 5, 6]
+
+    if not c:
+        c = [0]
+
+    n_neurons, n_bins = spike_trains.shape
+
+    filter_pairs = generate_filter_pairs(a, b, c)
+
+    # Calculate normalized cross corelation for different delays
+    # The delay range ranges from 0 to max-delay and includes
+    # padding for the filter convolution
+    max_padding = max(a) + max(c)
+    delay_times = list(range(-max_padding,max_delay + max_padding))
+    NCC_d = normalized_cross_correlation(spike_trains, delay_times=delay_times)
+
+    # Normalize to counter network-bursts
+    if normalize:
+        for delay_time in delay_times:
+            NCC_d[:,:,delay_time] /= np.sum(NCC_d[:,:,delay_time][~np.identity(NCC_d.shape[0],dtype=bool)])
+
+    # Apply edge and running total filter
+    tspe_matrix = np.zeros((n_neurons, n_neurons, max_delay))
+    for filter in filter_pairs:
+        # Select ncc_window based on needed filter padding
+        NCC_window = NCC_d[:,:,max_padding-filter.needed_padding:max_delay+max_padding+filter.needed_padding]
+
+        # Compute two convolutions with edge- and running total filter
+        x1 = oaconvolve(NCC_window, np.expand_dims(filter.edge_filter,(0,1)), mode="valid",axes=2)
+        x2 = oaconvolve(x1, np.expand_dims(filter.running_total_filter,(0,1)), mode="full",axes=2)
+
+        tspe_matrix += x2
 
     return tspe_matrix
 
@@ -88,10 +132,6 @@ def normalized_cross_correlation(
     NCC_d = np.moveaxis(NCC_d, 0, -1)
 
     return NCC_d
-
-
-def filter_edges():
-    pass
 
 
 def generate_edge_filter(
