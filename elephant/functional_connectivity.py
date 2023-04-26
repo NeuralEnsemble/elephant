867bab7a0b57547a133c781b7bb56d4e1ad7d6d8
Felician Richter <f.richter@tiplu.de>
2023-04-26 02:56:34 +0200

    Feat: Add function to compute connectivity_matrix

--- elephant/functional_connectivity.py
+++ elephant/functional_connectivity.py
@@ -1,5 +1,6 @@
-from elephant.functional_connectivity_src.total_spiking_probability_edges import total_spiking_probability_edges
+from elephant.functional_connectivity_src.total_spiking_probability_edges import (
+    total_spiking_probability_edges,
+    get_connectivity_matrix,
+)
 
-__all__ = [
-    "total_spiking_probability_edges"
-]
+__all__ = ["total_spiking_probability_edges", "get_connectivity_matrix"]
