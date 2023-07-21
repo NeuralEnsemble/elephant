8b9e0f992e32eafa119ed0cfc5a4fed6b6765fc7
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2023-07-21 13:31:56 +0200

    fixed pep8 issue in test_total_spiking_probability_edges.py

--- elephant/test/test_total_spiking_probability_edges.py
+++ elephant/test/test_total_spiking_probability_edges.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional, Tuple, Union
+from typing import Tuple, Union
 
 from neo import SpikeTrain
 import numpy as np
@@ -8,12 +8,12 @@ from quantities import millisecond as ms
 from scipy.io import loadmat
 
 from elephant.conversion import BinnedSpikeTrain
-from elephant.functional_connectivity_src.total_spiking_probability_edges import (
-    generate_filter_pairs,
-    normalized_cross_correlation,
-    TspeFilterPair,
-    total_spiking_probability_edges,
-)
+from elephant.functional_connectivity_src.total_spiking_probability_edges \
+    import (generate_filter_pairs,
+            normalized_cross_correlation,
+            TspeFilterPair,
+            total_spiking_probability_edges,
+            )
 
 from elephant.datasets import download_datasets
 
@@ -35,7 +35,8 @@ def test_generate_filter_pairs():
 
     function_output = generate_filter_pairs(a, b, c)
 
-    for filter_pair_function, filter_pair_test in zip(function_output, test_output):
+    for filter_pair_function, filter_pair_test in zip(function_output,
+                                                      test_output):
         assert np.array_equal(
             filter_pair_function.edge_filter, filter_pair_test.edge_filter
         )
@@ -43,7 +44,8 @@ def test_generate_filter_pairs():
             filter_pair_function.running_total_filter,
             filter_pair_test.running_total_filter,
         )
-        assert filter_pair_function.needed_padding == filter_pair_test.needed_padding
+        assert filter_pair_function.needed_padding == \
+               filter_pair_test.needed_padding
         assert (
             filter_pair_function.surrounding_window_size
             == filter_pair_test.surrounding_window_size
@@ -72,7 +74,8 @@ def test_normalized_cross_correlation():
         bin_size=1 * ms,
     )
 
-    test_output = np.array([[[0.0, 0.0], [1.1, 0.0]], [[0.0, 1.1], [0.0, 0.0]]])
+    test_output = np.array([[[0.0, 0.0], [1.1, 0.0]], [[0.0, 1.1], [0.0, 0.0]]]
+                           )
 
     function_output = normalized_cross_correlation(
         spiketrains, [-delay_time, delay_time]
@@ -82,7 +85,8 @@ def test_normalized_cross_correlation():
 
 # ====== HELPER FUNCTIONS ======
 
-def classify_connections(connectivity_matrix:np.ndarray,threshold:int):
+
+def classify_connections(connectivity_matrix: np.ndarray, threshold: int):
     connectivity_matrix_binarized = connectivity_matrix.copy()
 
     mask_excitatory = connectivity_matrix_binarized > threshold
@@ -96,6 +100,7 @@ def classify_connections(connectivity_matrix:np.ndarray,threshold:int):
 
     return connectivity_matrix_binarized
 
+
 def confusion_matrix(estimate, original, threshold: int = 1):
     """
     Definition:
@@ -104,39 +109,42 @@ def confusion_matrix(estimate, original, threshold: int = 1):
         - TN: Matches for non-existing synapses are True Negative
         - FN: mismatches are False Negative.
     """
-    if not np.all(np.isin([-1,0,1], np.unique(estimate))):
-        estimate = classify_connections(estimate,threshold)
-    if not np.all(np.isin([-1,0,1], np.unique(original))):
-        original = classify_connections(original,threshold)
+    if not np.all(np.isin([-1, 0, 1], np.unique(estimate))):
+        estimate = classify_connections(estimate, threshold)
+    if not np.all(np.isin([-1, 0, 1], np.unique(original))):
+        original = classify_connections(original, threshold)
 
-    TP = (np.not_equal(estimate,0) & np.not_equal(original,0)).sum()
+    TP = (np.not_equal(estimate, 0) & np.not_equal(original, 0)).sum()
 
-    TN = (np.equal(estimate,0) & np.equal(original, 0)).sum()
+    TN = (np.equal(estimate, 0) & np.equal(original, 0)).sum()
 
-    FP = (np.not_equal(estimate,0) & np.equal(original, 0)).sum()
+    FP = (np.not_equal(estimate, 0) & np.equal(original, 0)).sum()
 
-    FN = (np.equal(estimate, 0) & np.not_equal(original,0)).sum()
+    FN = (np.equal(estimate, 0) & np.not_equal(original, 0)).sum()
 
     return TP, TN, FP, FN
 
-def fall_out(TP:int, TN:int, FP:int, FN:int):
+
+def fall_out(TP: int, TN: int, FP: int, FN: int):
     FPR = FP / (FP + TN)
     return FPR
 
-def sensitivity(TP:int, TN:int, FP:int, FN:int):
+
+def sensitivity(TP: int, TN: int, FP: int, FN: int):
     TPR = TP / (TP + FN)
     return TPR
 
-def roc_curve(estimate,original):
+
+def roc_curve(estimate, original):
     tpr_list = []
     fpr_list = []
 
-    max_threshold = max(np.max(np.abs(estimate)),1)
+    max_threshold = max(np.max(np.abs(estimate)), 1)
 
-    thresholds = np.linspace(max_threshold,0,30)
+    thresholds = np.linspace(max_threshold, 0, 30)
 
     for t in thresholds:
-        conf_matrix = confusion_matrix(estimate,original,threshold=t)
+        conf_matrix = confusion_matrix(estimate, original, threshold=t)
 
         tpr_list.append(sensitivity(*conf_matrix))
         fpr_list.append(fall_out(*conf_matrix))
@@ -146,11 +154,9 @@ def roc_curve(estimate,original):
     return tpr_list, fpr_list, thresholds, auc
 
 
-def load_spike_train_simulated(
-    path: Union[Path, str],
-    bin_size = None,
-    t_stop = None,
-) -> Tuple[BinnedSpikeTrain, np.ndarray]:
+def load_spike_train_simulated(path: Union[Path, str], bin_size=None,
+                               t_stop=None,
+                               ) -> Tuple[BinnedSpikeTrain, np.ndarray]:
     if isinstance(path, str):
         path = Path(path)
 
@@ -160,11 +166,13 @@ def load_spike_train_simulated(
     data = loadmat(path, simplify_cells=True)["data"]
 
     if "asdf" not in data:
-        raise ValueError('Incorrect Dataformat: Missing spiketrain_data in "asdf"')
+        raise ValueError('Incorrect Dataformat: Missing spiketrain_data in'
+                         '"asdf"')
 
     spiketrain_data = data["asdf"]
 
-    # Get number of electrodesa and recording_duration from last element of data array
+    # Get number of electrodesa and recording_duration from last element of
+    # data array
     n_electrodes, recording_duration_ms = spiketrain_data[-1]
     recording_duration_ms = recording_duration_ms * ms
 
@@ -174,11 +182,12 @@ def load_spike_train_simulated(
         spiketrains.append(
             SpikeTrain(
                 spiketrain_raw * ms,
-                t_stop= recording_duration_ms,
+                t_stop=recording_duration_ms,
             )
         )
 
-    spiketrains = BinnedSpikeTrain(spiketrains, bin_size=bin_size, t_stop = t_stop or recording_duration_ms)
+    spiketrains = BinnedSpikeTrain(spiketrains, bin_size=bin_size,
+                                   t_stop=t_stop or recording_duration_ms)
 
     # Load original_data
     original_data = data['SWM'].T
@@ -199,12 +208,15 @@ def load_spike_train_simulated(
 )
 def test_total_spiking_probability_edges(datafile):
 
-    repo_base_path = 'unittest/functional_connectivity/total_spiking_probability_edges/data/'
+    repo_base_path = 'unittest/functional_connectivity/' \
+                     'total_spiking_probability_edges/data/'
     downloaded_dataset_path = download_datasets(repo_base_path+datafile)
 
-    spiketrains, original_data = load_spike_train_simulated(downloaded_dataset_path)
+    spiketrains, original_data = load_spike_train_simulated(
+        downloaded_dataset_path)
 
-    connectivity_matrix, delay_matrix = total_spiking_probability_edges(spiketrains)
+    connectivity_matrix, delay_matrix = total_spiking_probability_edges(
+        spiketrains)
 
     # Remove self-connections
     np.fill_diagonal(connectivity_matrix, 0)
@@ -212,4 +224,3 @@ def test_total_spiking_probability_edges(datafile):
     _, _, _, auc = roc_curve(connectivity_matrix, original_data)
 
     assert auc > 0.95
-
