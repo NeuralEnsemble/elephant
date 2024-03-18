0f5083db14eb389a15461ae792c842fd562e6df3
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2024-03-18 16:09:32 +0100

    remove space

--- elephant/test/test_spike_train_synchrony.py
+++ elephant/test/test_spike_train_synchrony.py
@@ -445,9 +445,6 @@ class SynchrofactDetectionTestCase(unittest.TestCase):
         correct_array_annotations = {key: value[correct_mask] for key, value in
                                      spiketrain.array_annotations.items()}
 
-        # block contains only one spiketrain before synchrofact removal.
-        self.assertEqual(len(block.filter(objects=neo.SpikeTrain)), 1)
-
         # perform a synchrofact search with delete=True
         synchrofact_obj = Synchrotool(
             [spiketrain],
@@ -459,7 +456,7 @@ class SynchrofactDetectionTestCase(unittest.TestCase):
             in_place=True,
             threshold=2)
 
-        # Ensure that the spiketrain was not duplicated.
+        # Ensure that the spiketrain was not duplicated
         self.assertEqual(len(block.filter(objects=neo.SpikeTrain)), 1)
 
         cleaned_spiketrain = segment.spiketrains[0]
