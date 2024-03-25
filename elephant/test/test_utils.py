c3034346ec71466f293e0af22c51de70ee7c8d3a
Moritz-Alexander-Kern <moritz.kern@ymail.com>
2024-03-25 16:56:34 +0100

    add unit tests for new decorator

--- elephant/test/test_utils.py
+++ elephant/test/test_utils.py
@@ -12,6 +12,11 @@ import quantities as pq
 from elephant import utils
 from numpy.testing import assert_array_equal
 
+from elephant.spike_train_generation import StationaryPoissonProcess
+from elephant.trials import TrialsFromLists
+from neo.core.spiketrainlist import SpikeTrainList
+from neo.core import SpikeTrain
+
 
 class TestUtils(unittest.TestCase):
 
@@ -54,6 +59,78 @@ class TestUtils(unittest.TestCase):
         assert_array_equal(
             utils.round_binning_errors(array.copy(), tolerance=None),
             [0, 0, 0, 0])
+
+
+class DecoratorTest:
+    @utils.trials_to_list_of_spiketrainlist
+    def method_to_decorate(self, trials=None, trials_obj=None):
+        # This is just a mock implementation for testing purposes
+        if trials_obj:
+            return trials_obj
+        else:
+            return trials
+
+
+class TestTrialsToListOfSpiketrainlist(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        n_channels = 10
+        n_trials = 5
+        cls.list_of_list_of_spiketrains = [
+            StationaryPoissonProcess(rate=5 * pq.Hz, t_stop=1000.0 * pq.ms
+                                     ).generate_n_spiketrains(n_channels)
+            for _ in range(n_trials)]
+        cls.trial_object = TrialsFromLists(cls.list_of_list_of_spiketrains)
+
+    def test_decorator_applied(self):
+        # Test that the decorator is applied correctly
+        self.assertTrue(hasattr(
+            DecoratorTest.method_to_decorate, '__wrapped__'
+            ))
+
+    def test_decorator_return_with_trials_input_as_arg(self):
+        # Test if decorator takes in trial-object and returns
+        # list of spiketrainlists
+        new_class = DecoratorTest()
+        list_of_spiketrainlists = new_class.method_to_decorate(
+            self.trial_object)
+        self.assertEquals(len(list_of_spiketrainlists), 5)
+        for spiketrainlist in list_of_spiketrainlists:
+            self.assertIsInstance(spiketrainlist, SpikeTrainList)
+
+    def test_decorator_return_with_list_of_lists_input_as_arg(self):
+        # Test if decorator takes in list of lists of spiketrains
+        # and does not change input
+        new_class = DecoratorTest()
+        list_of_list_of_spiketrains = new_class.method_to_decorate(
+            self.list_of_list_of_spiketrains)
+        self.assertEquals(len(list_of_list_of_spiketrains), 5)
+        for list_of_spiketrains in list_of_list_of_spiketrains:
+            self.assertIsInstance(list_of_spiketrains, list)
+            for spiketrain in list_of_spiketrains:
+                self.assertIsInstance(spiketrain, SpikeTrain)
+
+    def test_decorator_return_with_trials_input_as_kwarg(self):
+        # Test if decorator takes in trial-object and returns
+        # list of spiketrainlists
+        new_class = DecoratorTest()
+        list_of_spiketrainlists = new_class.method_to_decorate(
+            trials_obj=self.trial_object)
+        self.assertEquals(len(list_of_spiketrainlists), 5)
+        for spiketrainlist in list_of_spiketrainlists:
+            self.assertIsInstance(spiketrainlist, SpikeTrainList)
+
+    def test_decorator_return_with_list_of_lists_input_as_kwarg(self):
+        # Test if decorator takes in list of lists of spiketrains
+        # and does not change input
+        new_class = DecoratorTest()
+        list_of_list_of_spiketrains = new_class.method_to_decorate(
+            trials_obj=self.list_of_list_of_spiketrains)
+        self.assertEquals(len(list_of_list_of_spiketrains), 5)
+        for list_of_spiketrains in list_of_list_of_spiketrains:
+            self.assertIsInstance(list_of_spiketrains, list)
+            for spiketrain in list_of_spiketrains:
+                self.assertIsInstance(spiketrain, SpikeTrain)
 
 
 if __name__ == '__main__':
