ee383b8262f2146e7b92465e696cf589f701ba6e
morales-gregorio <aitormorales95@gmail.com>
2024-04-04 12:07:30 +0200

    fix new error for metadata update with spiketrainlist

--- elephant/spike_train_synchrony.py
+++ elephant/spike_train_synchrony.py
@@ -356,7 +356,12 @@ class Synchrotool(Complexity):
                     # replace link to spiketrain in segment
                     new_index = self._get_spiketrain_index(
                         segment.spiketrains, st)
-                    segment.spiketrains[new_index] = new_st
+
+                    # explicitly re-define the spiketrains
+                    sts = list(segment.spiketrains)
+                    sts[new_index] = new_st
+                    segment.spiketrains = sts
+
                 except ValueError:
                     # st is not in this segment even though it points to it
                     warnings.warn(f"The SpikeTrain at index {idx} of the "
