a2d2a9317bd5a148586d2726170c6a4d172d3365
Moritz-Alexander-Kern <92092328+Moritz-Alexander-Kern@users.noreply.github.com>
2024-07-26 14:40:42 +0200

    change t_stop back

--- elephant/spike_train_synchrony.py
+++ elephant/spike_train_synchrony.py
@@ -20,7 +20,7 @@ from __future__ import division, print_function, unicode_literals
 
 import warnings
 from collections import namedtuple
-from copy import deepcopy
+from copy import deepcopy, copy
 
 import neo
 import numpy as np
@@ -279,6 +279,7 @@ class Synchrotool(Complexity):
                  include_t_stop=True):
 
         self.annotated = False
+        initial_t_stop = copy(spiketrains[0].t_stop)
 
         super(Synchrotool, self).__init__(
             spiketrains=spiketrains,
@@ -289,6 +290,9 @@ class Synchrotool(Complexity):
             tolerance=tolerance,
             t_stop=spiketrains[0].t_stop + (1 / sampling_rate) if include_t_stop else None,
             )
+
+        for st in spiketrains:
+            st.t_stop = initial_t_stop
 
     def delete_synchrofacts(self, threshold, in_place=False, mode='delete'):
         """
