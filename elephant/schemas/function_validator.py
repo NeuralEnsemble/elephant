c46228df38394903f4766114ffd5f4f6669d2aa6
Jan Nolten <j.nolten@fz-juelich.de>
2025-11-24 13:25:13 +0100

    Removed the option to skip validation with the extra kwargs not_validate, so the api does not need to be changed and to simplify the decorator

--- elephant/schemas/function_validator.py
+++ elephant/schemas/function_validator.py
@@ -15,21 +15,17 @@ def validate_with(model_class: type[BaseModel]):
         @wraps(func)
         def wrapper(*args, **kwargs):
 
-            if kwargs.pop("not_validate", False) or skip_validation:
-            # skip validation, call inner function directly
-                return func(*args, **kwargs)
+            if not skip_validation:
+                # Bind args & kwargs to function parameters
+                bound = sig.bind_partial(*args, **kwargs)
+                bound.apply_defaults()
+                data = bound.arguments
 
-            # Bind args & kwargs to function parameters
-            bound = sig.bind_partial(*args, **kwargs)
-            bound.apply_defaults()
-            data = bound.arguments
-
-            # Validate using Pydantic
-            model_class(**data)
+                # Validate using Pydantic
+                model_class(**data)
 
             # Call function
             return func(*args, **kwargs)
-        wrapper._is_validate_with = True
         return wrapper
     return decorator
 
