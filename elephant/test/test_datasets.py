c932bc2aff5351d1619c76ff656e8f97eae55997
Cristiano Köhler <cristiano.andre.kohler@gmail.com>
2026-02-13 00:18:11 +0100

    Added separate tests for cached data in a local folder

--- elephant/test/test_datasets.py
+++ elephant/test/test_datasets.py
@@ -11,9 +11,20 @@ from neo import NixIO
 from elephant.datasets import download_datasets, load_data, ELEPHANT_DATA
 
 
+# Most tests are done with mocking the environment variable to test different
+# scenarios. However, in CI tests, ELEPHANT_DATA_LOCATION is set to a local
+# cache path of the "elephant-data" repository, which is expected to be a
+# folder. We use this for additional tests with the cache directory.
+# HAS_CACHE_DIR is used as a flag to conditionally run tests that require the
+# cache directory.
+HAS_CACHE_DIR = ("ELEPHANT_DATA_LOCATION" in os.environ
+                 and Path(os.environ["ELEPHANT_DATA_LOCATION"]).is_dir())
+
+
 class TestDownloadDatasets(unittest.TestCase):
 
-    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': '/invalid/path'}, clear=True)
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': '/invalid/path'},
+                clear=True)
     def test_invalid_path(self):
         repo_path = 'some/repo/path'
         with self.assertRaises(ValueError) as error:
@@ -34,16 +45,19 @@ class TestDownloadDatasets(unittest.TestCase):
                       exception_msg)
         self.assertIn("ftp://invalid.url", exception_msg)
 
-    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': 'http://valid.url'}, clear=True)
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': 'http://valid.url'},
+                clear=True)
     def test_valid_url(self):
         repo_path = 'some/repo/path'
         self.assertRaises(urllib.error.URLError, download_datasets, repo_path)
 
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': ''}, clear=True)
     def test_invalid_data(self):
         repo_path = 'some/repo/path'
         self.assertRaises(urllib.error.URLError, download_datasets, repo_path)
 
-    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': 'invalid_path_or_url'}, clear=True)
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': 'invalid_path_or_url'},
+                clear=True)
     def test_invalid_value(self):
         repo_path = 'some/repo/path'
         with self.assertRaises(ValueError) as error:
@@ -53,6 +67,8 @@ class TestDownloadDatasets(unittest.TestCase):
                       exception_msg)
         self.assertIn("invalid_path_or_url", exception_msg)
 
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': ''},
+                clear=True)
     def test_valid_data(self):
         # This is expected to download a file with the same name in GIN to a
         # folder 'elephant' within the current system temporary folder
@@ -67,6 +83,8 @@ class TestDownloadDatasets(unittest.TestCase):
         self.assertTrue(Path(downloaded_file).is_file())
         self.assertEqual(dummy_file_path, downloaded_file)
 
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': ''},
+                clear=True)
     def test_valid_data_with_path(self):
         # This is expected to download a file to the provided path
         # (using a temporary directory specific to the test)
@@ -80,6 +98,8 @@ class TestDownloadDatasets(unittest.TestCase):
             self.assertTrue(Path(downloaded_file).is_file())
             self.assertEqual(dummy_file_path, downloaded_file)
 
+    @patch.dict(os.environ, {'ELEPHANT_DATA_LOCATION': ''},
+                clear=True)
     def test_valid_data_with_integrity_check(self):
         # This is expected to check the integrity of the downloaded file by
         # comparing the MD5 hash. We manually download the file to the current
@@ -118,6 +138,59 @@ class TestDownloadDatasets(unittest.TestCase):
             self.assertIn(repo_path, exception_msg)
             self.assertIn("does not agree with MD5 hash aaaaaa", exception_msg)
 
+    @unittest.skipIf(not HAS_CACHE_DIR,
+                     "Not testing since ELEPHANT_DATA_LOCATION is not set to "
+                     "a folder")
+    def test_download_with_cache_dir(self):
+        # This test is expected in the GitHub Actions environment where the
+        # variable is set to a local cache path. This tests that the function
+        # correctly uses the cache directory and returns the expected file
+        # path without re-downloading.
+        repo_path = 'dataset-1/dataset-1.h5'
+
+        # Expected path of the downloaded dataset
+        expected_file_path = (
+                Path(os.environ['ELEPHANT_DATA_LOCATION']) /
+                "dataset-1" / "dataset-1.h5")
+
+        downloaded_file = download_datasets(repo_path,
+                                            filepath=None,
+                                            checksum=None)
+        self.assertTrue(Path(downloaded_file).is_file())
+        self.assertEqual(expected_file_path, downloaded_file)
+
+    @unittest.skipIf(not HAS_CACHE_DIR,
+                     "Not testing since ELEPHANT_DATA_LOCATION is not set to "
+                     "a folder")
+    def test_download_with_cache_dir_target_path(self):
+        # This test is expected in the GitHub Actions environment where the
+        # variable is set to a local cache path. This tests if the function
+        # uses the cache directory and correctly copies the file to the
+        # provided target path.
+        repo_path = 'dataset-1/dataset-1.h5'
+        with TemporaryDirectory() as temp_dir:
+            target_file_path = Path(temp_dir) / 'target_cache'
+            downloaded_file = download_datasets(repo_path,
+                                                filepath=target_file_path,
+                                                checksum=None)
+            self.assertTrue(Path(downloaded_file).is_file())
+            self.assertEqual(target_file_path, downloaded_file)
+
+    @unittest.skipIf(not HAS_CACHE_DIR,
+                     "Not testing since ELEPHANT_DATA_LOCATION is not set to "
+                     "a folder")
+    def test_download_with_cache_dir_invalid_file(self):
+        # This test is expected in the GitHub Actions environment where the
+        # variable is set to a local cache path. This test the behavior when
+        # usinga cache dir and requesting a file that does not exist.
+        repo_path = 'dataset-1/not-existent'
+        with self.assertRaises(ValueError) as error:
+            download_datasets(repo_path)
+        exception_msg = str(error.exception)
+        self.assertIn("ELEPHANT_DATA_LOCATION is set to the local path",
+                      exception_msg)
+        self.assertIn(f"'{repo_path}' does not exist in that path",
+                      exception_msg)
 
 class TestLoadData(unittest.TestCase):
 
